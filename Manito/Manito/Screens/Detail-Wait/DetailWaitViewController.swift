0d08ea5b92711da1426a8841ede2254f118713e3
chemi <wbsldj7645@naver.com>
2022-06-09 21:43:51 +0900

    [ADD] 대기중 타이틀 추가 (#1)

--- Manito/Manito/Screens/Detail-Wait/DetailWaitViewController.swift
+++ Manito/Manito/Screens/Detail-Wait/DetailWaitViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class DetailWaitViewController: UIViewController {
+    @IBOutlet weak var roomTitle: UILabel!
+    @IBOutlet weak var startStatus: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        roomTitle.textColor = .white
+        roomTitle.font = UIFont(name: "DungGeunMo", size: 34)
+
+        // MARK: 강제로 맞춘거라 어떻게 해야 유연하게 바뀌는지 모르겠음
+        startStatus.frame = CGRect(x: 200, y: 130, width: 66, height: 23)
+        startStatus.backgroundColor = .waitBackgroundColor
+        startStatus.layer.masksToBounds = true
+        startStatus.layer.cornerRadius = 11
+        startStatus.textColor = .waitTextColor
+        startStatus.font = UIFont(name: "DungGeunMo", size: 13)
+        startStatus.textAlignment = .center
 
         // Do any additional setup after loading the view.
     }
