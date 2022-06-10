aa0b8927fe1321054fee1bfca04625b4eff1a51f
chemi <wbsldj7645@naver.com>
2022-06-10 15:57:20 +0900

    [Chore] 대기중View 코드베이스로 변경 (#1)
    - 타이틀, 현재상태 수정

--- Manito/Manito/Screens/Detail-Wait/DetailWaitViewController.swift
+++ Manito/Manito/Screens/Detail-Wait/DetailWaitViewController.swift
@@ -6,56 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
-class DetailWaitViewController: UIViewController {
-    @IBOutlet weak var roomTitle: UILabel!
-    @IBOutlet weak var startStatus: UILabel!
-    @IBOutlet weak var durationView: UIView!
-    @IBOutlet weak var durationText: UILabel!
-    @IBOutlet weak var durationDateText: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .backgroundColor
-        setRoomTitle(label: roomTitle)
-        setStartStatus(label: startStatus)
-        setDurationView(view: durationView)
-        setDurationTitle(label: durationText)
-        setDurationDateText(label: durationDateText)
-
-        // Do any additional setup after loading the view.
-    }
-
-    func setRoomTitle(label: UILabel) {
+class DetailWaitViewController: BaseViewController {
+    private let roomTitle: UILabel = {
+        let label = UILabel()
+        label.text = "명예소방관"
         label.textColor = .white
         label.font = UIFont(name: AppFontName.regular.rawValue, size: 34)
-    }
-    
-    func setStartStatus(label: UILabel) {
+        return label
+    }()
+
+    private let startStauts: UILabel = {
+        let label = UILabel()
+        // MARK: enum으로 만드는게 좋아보임
+        label.text = "대기중"
         label.backgroundColor = .waitBackgroundColor
         label.layer.masksToBounds = true
         label.layer.cornerRadius = 11
         label.textColor = .waitTextColor
         label.font = UIFont(name: AppFontName.regular.rawValue, size: 13)
         label.textAlignment = .center
-    }
-    
-    func setDurationView(view: UIView) {
-        view.backgroundColor = .durationBackgroundColor
-        view.layer.cornerRadius = 8
-    }
-    
-    func setDurationTitle(label: UILabel) {
-        label.textColor = .grey4
-        label.font = UIFont(name: AppFontName.regular.rawValue, size: 14)
-    }
-    
-    func setDurationDateText(label: UILabel) {
-        label.textColor = .white
-        label.font = UIFont(name: AppFontName.regular.rawValue, size: 18)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .backgroundColor
+        render()
+        // Do any additional setup after loading the view.
     }
 
+    override func render() {
+        view.addSubview(roomTitle)
+        roomTitle.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(16)
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(30)
+        }
 
+        view.addSubview(startStauts)
+        startStauts.snp.makeConstraints {
+            $0.centerY.equalTo(roomTitle.snp.centerY)
+            $0.left.equalTo(roomTitle.snp.right).inset(-10)
+            $0.width.equalTo(66)
+            $0.height.equalTo(23)
+        }
+    }
 
     /*
     // MARK: - Navigation
