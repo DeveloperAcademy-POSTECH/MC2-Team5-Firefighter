9d1e475d011119e2355c463050388328bb779486
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-11 16:52:42 +0900

    [CHORE] HeaderView Reusable View로 변경(#6)

--- Manito/Manito/Screens/Letter/LetterViewController.swift
+++ Manito/Manito/Screens/Letter/LetterViewController.swift
@@ -11,42 +11,34 @@ import SnapKit
 
 final class LetterViewController: BaseViewController {
     
-    private enum LetterState: String, CaseIterable {
-        case received = "받은 쪽지"
-        case sent = "보낸 쪽지"
+    private enum Size {
+        static let headerHeight: CGFloat = 70.0
     }
     
     // MARK: - property
     
-    private let segmentControl: UISegmentedControl = {
-        let control = UISegmentedControl(items: [LetterState.received.rawValue,
-                                                 LetterState.sent.rawValue])
-        let normalTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        let selectedTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
-        
-        control.setTitleTextAttributes(normalTextAttributes, for: .normal)
-        control.setTitleTextAttributes(selectedTextAttributes, for: .selected)
-        control.selectedSegmentTintColor = .white
-        control.backgroundColor = .darkGrey003
-        control.selectedSegmentIndex = 0
-        
-        return control
+    private let collectionViewFlowLayout: UICollectionViewFlowLayout = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        return flowLayout
+    }()
+    private lazy var listCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+        collectionView.backgroundColor = .clear
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(LetterCollectionReusableView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: LetterCollectionReusableView.className)
+        return collectionView
     }()
     
-    let button = UIButton()
-
     // MARK: - life cycle
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     override func render() {
-        view.addSubview(segmentControl)
-        segmentControl.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).inset(13)
-            $0.leading.trailing.equalToSuperview().inset(16)
-            $0.height.equalTo(40)
+        view.addSubview(listCollectionView)
+        listCollectionView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
     
@@ -56,12 +48,37 @@ final class LetterViewController: BaseViewController {
     
     override func setupNavigationBar() {
         super.setupNavigationBar()
-  
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .automatic
         title = "쪽지함"
     }
+}
+
+// MARK: - UICollectionViewDataSource
+extension LetterViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
     
-    // MARK: - func
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: LetterCollectionReusableView.className, for: indexPath) as? LetterCollectionReusableView else { assert(false, "do not have reusable view") }
+            return headerView
+        default:
+            assert(false, "do not use footer")
+        }
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension LetterViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.size.width, height: Size.headerHeight)
+    }
 }
---
+++ Manito/Manito/Screens/Letter/UIComponent/LetterCollectionReusableView.swift
@@ -0,0 +1,55 @@
+//
+//  LetterCollectionReusableView.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 2022/06/11.
+//
+
+import UIKit
+
+final class LetterCollectionReusableView: UICollectionReusableView {
+    
+    private enum LetterState: String, CaseIterable {
+        case received = "받은 쪽지"
+        case sent = "보낸 쪽지"
+    }
+    
+    // MARK: - property
+    
+    private let segmentControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: [LetterState.received.rawValue,
+                                                 LetterState.sent.rawValue])
+        let normalTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        let selectedTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        
+        control.setTitleTextAttributes(normalTextAttributes, for: .normal)
+        control.setTitleTextAttributes(selectedTextAttributes, for: .selected)
+        control.selectedSegmentTintColor = .white
+        control.backgroundColor = .darkGrey003
+        control.selectedSegmentIndex = 0
+        
+        return control
+    }()
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()g
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - func
+    
+    private func render() {
+        self.addSubview(segmentControl)
+        segmentControl.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(13)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(40)
+        }
+    }
+}
