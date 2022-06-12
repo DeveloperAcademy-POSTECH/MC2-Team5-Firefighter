5cda6e4fe14edf4a568d2a6bf40f7b518a6b3cda
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-12 15:19:46 +0900

    [ADD] 내부 셀 Label 구성 완료(#13)

--- Manito/Manito/Screens/Letter/Cell/LetterCollectionViewCell.swift
+++ Manito/Manito/Screens/Letter/Cell/LetterCollectionViewCell.swift
@@ -11,18 +11,53 @@ final class LetterCollectionViewCell: BaseCollectionViewCell {
     
     // MARK: - property
     
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .font(.regular, ofSize: 14)
+        label.textColor = .darkGrey005
+        return label
+    }()
+    private let contentLabel: UILabel = {
+        let label = UILabel()
+        label.font = .font(.regular, ofSize: 15)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.lineBreakMode = .byCharWrapping
+        return label
+    }()
+    
     // MARK: - init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        // FIXME: - 더미 데이터 추가해둠
+        dateLabel.text = "오늘"
+        contentLabel.text = """
+        오늘 구름을 봤는데 고양이를 너무 닮아서
+        귀여워서 보내드려요~ 오늘 하루도 화이팅!!
+        """
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func render() {
+        contentView.addSubview(dateLabel)
+        dateLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(14)
+            $0.trailing.equalToSuperview().inset(15)
+        }
+        
+        contentView.addSubview(contentLabel)
+        contentLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(39)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
+    
     override func configUI() {
-        backgroundColor = .mainRed
         makeBorderLayer(color: .white.withAlphaComponent(0.5))
     }
 }
