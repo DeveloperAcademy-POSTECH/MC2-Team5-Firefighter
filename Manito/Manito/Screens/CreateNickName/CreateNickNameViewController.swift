dcef0e4da18897f74aae0213f8a15e4ad09327af
LEE SUNG HO <sholee0508@naver.com>
2022-06-12 14:40:28 +0900

    [ADD] CreateNickName View init commit(#19)

---
+++ Manito/Manito/Screens/CreateNickName/CreateNickNameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreateNickNameViewController.swift
+//  Manito
+//
+//  Created by LeeSungHo on 2022/06/12.
+//
+
+import UIKit
+
+class CreateNickNameViewController: BaseViewController {
+    
+    // MARK: - Property
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    override func render() {
+        
+    }
+    
+    // MARK: - Configure
+    
+    override func configUI() {
+        
+    }
+    
+    // MARK: - Function
+    
+    
+    // MARK: - Selectors
+    
+    
+    // MARK: - Extension
+
+}
