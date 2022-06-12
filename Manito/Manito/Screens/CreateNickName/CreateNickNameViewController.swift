66966af4d6dc2d9e4957d49b5fa67c65ac21c758
LEE SUNG HO <sholee0508@naver.com>
2022-06-12 14:58:37 +0900

    [ADD] 화면구성 (#19)

--- Manito/Manito/Screens/CreateNickName/CreateNickNameViewController.swift
+++ Manito/Manito/Screens/CreateNickName/CreateNickNameViewController.swift
@@ -7,10 +7,34 @@
 
 import UIKit
 
+import SnapKit
+
 class CreateNickNameViewController: BaseViewController {
     
     // MARK: - Property
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "닉네임 설정"
+        label.font = .font(.regular, ofSize: 34)
+        return label
+    }()
     
+    private let roomsNameTextField: UITextField = {
+        let texField = UITextField()
+        let attributes = [
+            NSAttributedString.Key.foregroundColor : UIColor.white,
+            NSAttributedString.Key.font : UIFont.font(.regular, ofSize: 18)
+        ]
+        texField.backgroundColor = UIColor.subBackgroundGrey
+        texField.attributedPlaceholder = NSAttributedString(string: "방 이름을 적어주세요", attributes:attributes)
+        
+        texField.layer.cornerRadius = 10
+        texField.layer.masksToBounds = true
+        texField.layer.borderWidth = 1
+        texField.layer.borderColor = UIColor.white.cgColor
+        texField.textAlignment = .center
+        return texField
+    }()
     
     // MARK: - Life Cycle
     
@@ -20,12 +44,26 @@ class CreateNickNameViewController: BaseViewController {
     }
     override func render() {
         
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(66)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
+        
+        view.addSubview(roomsNameTextField)
+        roomsNameTextField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(66)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.height.equalTo(60)
+        }
+        
+        
     }
     
     // MARK: - Configure
     
     override func configUI() {
-        
+        view.backgroundColor = .backgroundGrey
     }
     
     // MARK: - Function
