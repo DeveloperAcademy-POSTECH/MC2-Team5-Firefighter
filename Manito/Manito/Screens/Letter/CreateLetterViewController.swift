7db2276d83b12edfb1041fac7f7cda00440af662
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-13 17:10:31 +0900

    [ADD] 내부 ScrollView 추가(#33)

--- Manito/Manito/Screens/Letter/CreateLetterViewController.swift
+++ Manito/Manito/Screens/Letter/CreateLetterViewController.swift
@@ -36,6 +36,13 @@ final class CreateLetterViewController: BaseViewController {
         button.setTitleColor(.subBlue.withAlphaComponent(0.5), for: .highlighted)
         return button
     }()
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }()
+    private let scrollContentView = UIView()
     private let missionView = IndividualMissionView(mission: "1000원 이하의 선물 주고 인증샷 받기")
     private let letterTextView = LetterTextView()
     private let letterPhotoView = LetterPhotoView()
@@ -54,23 +61,36 @@ final class CreateLetterViewController: BaseViewController {
             $0.centerX.equalToSuperview()
         }
         
-        view.addSubview(missionView)
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        scrollView.addSubview(scrollContentView)
+        scrollContentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalTo(scrollView.snp.width)
+        }
+        
+        scrollContentView.addSubview(missionView)
         missionView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(25)
+            $0.top.equalToSuperview().offset(25)
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(100)
         }
         
-        view.addSubview(letterTextView)
+        scrollContentView.addSubview(letterTextView)
         letterTextView.snp.makeConstraints {
             $0.top.equalTo(missionView.snp.bottom).offset(32)
             $0.leading.trailing.equalToSuperview().inset(16)
         }
         
-        view.addSubview(letterPhotoView)
+        scrollContentView.addSubview(letterPhotoView)
         letterPhotoView.snp.makeConstraints {
             $0.top.equalTo(letterTextView.snp.bottom).offset(22)
             $0.leading.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(105)
         }
     }
     
