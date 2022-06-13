78862e8bb90193b1348b3946d823484e2b71403d
Doyoung <coby5502@gmail.com>
2022-06-13 11:31:15 +0900

    [ADD] 마니또 시작하기 버튼 스타일 완료

--- Manito/Manito/Screens/Main/Cell/CreateRoomCollectionViewCell.swift
+++ Manito/Manito/Screens/Main/Cell/CreateRoomCollectionViewCell.swift
@@ -5,8 +5,59 @@
 //  Created by COBY_PRO on 2022/06/13.
 //
 
+import SnapKit
 import UIKit
 
-class CreateRoomCollectionViewCell: NSObject {
+class CreateRoomCollectionViewCell: UICollectionViewCell{
+    static let identifier = "CreateRoomCollectionViewCell"
+    
+    // MARK: - property
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    
+    private var menu: UILabel = {
+        let label = UILabel()
+        label.text = "새로운 마니또 시작"
+        label.textColor = .grey003
+        label.font = .font(.regular, ofSize: 14)
+        return label
+    }()
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
 
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - func
+
+    func setupView(){
+        backgroundColor = .grey003.withAlphaComponent(0.3)
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.white.withAlphaComponent(0.5).cgColor
+        layer.cornerRadius = 10
+
+        addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(22)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(85)
+            $0.height.equalTo(85)
+        }
+        
+        addSubview(menu)
+        menu.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(22)
+            $0.centerX.equalToSuperview()
+        }
+    }
 }
