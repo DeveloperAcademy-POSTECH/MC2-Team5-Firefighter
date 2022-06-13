644b2ccb5fc0b6d03e300f7ccd0b8f639ddd7f96
sunchoi22 <sunchoi22@pos.idserve.net>
2022-06-13 17:09:27 +0900

    [CHORE] 컬렉션뷰 변경(#37)

--- Manito/Manito/Screens/Detail-Ing/Cell/FriendCollectionViewCell.swift
+++ Manito/Manito/Screens/Detail-Ing/Cell/FriendCollectionViewCell.swift
@@ -5,8 +5,19 @@
 //  Created by 최성희 on 2022/06/13.
 //
 
-import Cocoa
+import UIKit
 
 class FriendCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var friendListBackView: UIView!
+    @IBOutlet weak var friendListImageView: UIImageView!
+    @IBOutlet weak var friendListNameLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        print("아니에요")
+    }
 }
--- Manito/Manito/Screens/Detail-Ing/FriendListViewController.swift
+++ Manito/Manito/Screens/Detail-Ing/FriendListViewController.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class FriendListViewController: UIViewController {
     
+    @IBOutlet weak var friendListCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        friendListCollectionView.dataSource = self
+    }
+}
+
+extension FriendListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FriendCollectionViewCell.className, for: indexPath) as? FriendCollectionViewCell else { return UICollectionViewCell() }
+        return cell
     }
 }
