4f9d54cf61c5208e145e7af7641bb87dba1de54a
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-13 12:06:54 +0900

    [DEL] TitleLabel 삭제(#33)

--- Manito/Manito/Screens/Letter/CreateLetterViewController.swift
+++ Manito/Manito/Screens/Letter/CreateLetterViewController.swift
@@ -13,13 +13,7 @@ final class CreateLetterViewController: BaseViewController {
 
     
     // MARK: - property
-    
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "쪽지 작성하기"
-        label.font = .font(.regular, ofSize: 34)
-        return label
-    }()
+
     private let missionView = IndividualMissionView(mission: "1000원 이하의 선물 주고 인증샷 받기")
     private let letterTextView = LetterTextView()
     private let letterPhotoView = LetterPhotoView()
@@ -32,15 +26,9 @@ final class CreateLetterViewController: BaseViewController {
     }
     
     override func render() {
-        view.addSubview(titleLabel)
-        titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(77)
-            $0.leading.equalToSuperview().inset(23)
-        }
-        
         view.addSubview(missionView)
         missionView.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(37)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(54)
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(100)
         }
