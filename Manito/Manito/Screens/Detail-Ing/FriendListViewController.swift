9f4fa9bbc3161310e98245e975587800a878aa4f
sunchoi22 <sunchoi22@pos.idserve.net>
2022-06-13 12:09:56 +0900

    [ADD] 스토리보드 추가(#37)

---
+++ Manito/Manito/Screens/Detail-Ing/FriendListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FriendListViewController.swift
+//  Manito
+//
+//  Created by 최성희 on 2022/06/13.
+//
+
+import UIKit
+
+class FriendListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
