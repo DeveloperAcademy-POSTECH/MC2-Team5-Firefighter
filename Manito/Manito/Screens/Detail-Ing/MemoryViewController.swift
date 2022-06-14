306579118bc067b765248cc74c3b0fa7ed13c586
sunchoi22 <sunchoi22@pos.idserve.net>
2022-06-14 15:49:24 +0900

    [ADD] 함께 했던 기록 스토리보드 추가(#43)

---
+++ Manito/Manito/Screens/Detail-Ing/MemoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoryViewController.swift
+//  Manito
+//
+//  Created by 최성희 on 2022/06/14.
+//
+
+import UIKit
+
+class MemoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
