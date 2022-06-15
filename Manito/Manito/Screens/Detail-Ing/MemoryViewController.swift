c5b0d49781011ef597346abc4e3ce2b8fb475e83
sunchoi22 <sunchoi22@pos.idserve.net>
2022-06-15 22:30:04 +0900

    [ADD] 추억내용 폰트 추가(#43)

--- Manito/Manito/Screens/Detail-Ing/MemoryViewController.swift
+++ Manito/Manito/Screens/Detail-Ing/MemoryViewController.swift
@@ -8,14 +8,43 @@
 import UIKit
 
 class MemoryViewController: UIViewController {
-
+    @IBOutlet weak var memoryControl: UISegmentedControl!
+    @IBOutlet weak var memoryIconView: UIImageView!
+    @IBOutlet weak var memoryManitoLabel: UILabel!
+    @IBOutlet weak var fromManitiFirstView: UIView!
+    @IBOutlet weak var fromManitiSecondView: UIView!
+    @IBOutlet weak var fromManitiSecondLabel: UILabel!
+    @IBOutlet weak var fromManitiThirdView: UIView!
+    @IBOutlet weak var fromManitiForthView: UIView!
+    @IBOutlet weak var fromManitiForthLabel: UILabel!
+    @IBOutlet weak var manitiIconBackView: UIView!
+    @IBOutlet weak var manitiIconView: UIImageView!
+    @IBOutlet weak var manitiNickLabel: UILabel!
+    @IBOutlet var memoriIconBottomView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupFont()
+        setupViewLayer()
         // Do any additional setup after loading the view.
     }
     
-
+    private func setupFont() {
+    memoryManitoLabel.font = .font(.regular, ofSize: 15)
+    fromManitiSecondLabel.font = .font(.regular, ofSize: 14)
+    fromManitiForthLabel.font = .font(.regular, ofSize: 14)
+    manitiNickLabel.font = .font(.regular, ofSize: 30)
+    }
+    
+    private func setupViewLayer() {
+    fromManitiFirstView.makeBorderLayer(color: .white)
+    fromManitiSecondView.makeBorderLayer(color: .white)
+    fromManitiThirdView.makeBorderLayer(color: .white)
+    fromManitiForthView.makeBorderLayer(color: .white)
+    manitiIconBackView.layer.cornerRadius = 50
+    manitiIconView.layer.cornerRadius = 50
+    }
     /*
     // MARK: - Navigation
 
