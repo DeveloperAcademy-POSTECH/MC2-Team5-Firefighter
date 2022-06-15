881502e65687b9f9683328563eb4cf3f36fc8489
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-06-15 22:41:49 +0900

    [ADD] 이미지에셋 추가(#53)

--- Manito/Manito/Global/Literal/ImageLiteral.swift
+++ Manito/Manito/Global/Literal/ImageLiteral.swift
@@ -17,7 +17,33 @@ enum ImageLiterals {
     static var icExit: UIImage { .load(systemName: "rectangle.portrait.and.arrow.right") }
     static var icCamera: UIImage { .load(systemName: "camera.on.rectangle") }
     
+    // MARK: - button
+    
+    static var btnList: UIImage { .load(name: "btnList") }
+    static var btnManiTti: UIImage { .load(name: "btnManiTti") }
+    static var btnNewRoom: UIImage { .load(name: "btnNewRoom") }
+    static var btnSliderThumb: UIImage { .load(name: "btnSliderThumb") }
+    
     // MARK: - image
+    
+    static var imgLogo: UIImage { .load(name: "imgLogo") }
+    static var imgBackground: UIImage { .load(name: "imgBackground") }
+    static var imgStar: UIImage { .load(name: "imgStar") }
+    static var imgCodeBackground: UIImage { .load(name: "imgCodeBackground") }
+    static var imgCommonMisson: UIImage { .load(name: "imgCommonMisson") }
+    static var imgCheckMa: UIImage { .load(name: "imgCheckMa") }
+    static var imgCreateRoomPerson: UIImage { .load(name: "imgCreateRoomPerson") }
+    static var imgEnterRoom: UIImage { .load(name: "imgEnterRoom") }
+    static var imgRoomBaby: UIImage { .load(name: "imgRoomBaby") }
+    static var imgMa: UIImage { .load(name: "imgMa") }
+    static var imgNi: UIImage { .load(name: "imgNi") }
+    static var imgTto: UIImage { .load(name: "imgTto") }
+    static var imgMaChemi: UIImage { .load(name: "imgMaChemi") }
+    static var imgMaCoby: UIImage { .load(name: "imgMaCoby") }
+    static var imgMaDuna: UIImage { .load(name: "imgMaDuna") }
+    static var imgMaDinner: UIImage { .load(name: "imgMaDinner") }
+    static var imgMaHoya: UIImage { .load(name: "imgMaHoya") }
+    static var imgMaLivvy: UIImage { .load(name: "imgMaLivvy") }
     
 }
 
