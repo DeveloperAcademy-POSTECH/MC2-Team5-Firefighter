c34df124f6a0b6c4030cae7cf157168906a96604
Doyoung <coby5502@gmail.com>
2022-06-15 09:27:33 +0900

    [ADD] ParticipateRomm 관련 파일 추가

---
+++ Manito/Manito/Screens/ParticipateRoom/ParticipateRoomViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ParticipateRoomViewController.swift
+//  Manito
+//
+//  Created by COBY_PRO on 2022/06/15.
+//
+
+import UIKit
+
+import SnapKit
+
+class ParticipateRoomViewController: BaseViewController {
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
