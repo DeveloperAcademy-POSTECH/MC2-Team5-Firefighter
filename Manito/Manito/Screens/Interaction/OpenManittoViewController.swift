72d36377096a7cef729a1f3bb3f96a84c6db52d0
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-16 15:44:52 +0900

    [ADD] Interaction storyboard 추가(#62)

---
+++ Manito/Manito/Screens/Interaction/OpenManittoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OpenManittoViewController.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 2022/06/16.
+//
+
+import UIKit
+
+class OpenManittoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
