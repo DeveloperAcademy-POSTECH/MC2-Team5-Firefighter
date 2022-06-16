1b8db20b699c7d483c85d6071415b90893c6ed6d
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-16 10:08:52 +0900

    [ADD] Splash VC 파일 생성(#56)

---
+++ Manito/Manito/Screens/Splash/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 2022/06/16.
+//
+
+import UIKit
+
+final class SplashViewController: BaseViewController {
+
+    // MARK: - property
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
