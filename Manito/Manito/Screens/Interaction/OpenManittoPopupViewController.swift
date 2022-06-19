4746e8430ba7268e9528559b7eef4be2501c13d5
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-19 11:28:41 +0900

    [ADD] 바뀌지 않는 UI Component 추가(#68)

--- Manito/Manito/Screens/Interaction/OpenManittoPopupViewController.swift
+++ Manito/Manito/Screens/Interaction/OpenManittoPopupViewController.swift
@@ -14,6 +14,27 @@ final class OpenManittoPopupViewController: BaseViewController {
     // MARK: - property
     
     private let popupView = UIImageView(image: ImageLiterals.imgEnterRoom)
+    private let openMentLabel: UILabel = {
+        let label = UILabel()
+        label.text = """
+        함께 했던 추억이 열렸습니다.
+        마니또 방에서 확인해보세요!
+        """
+        label.numberOfLines = 2
+        label.font = .font(.regular, ofSize: 18)
+        label.addLabelSpacing()
+        label.textAlignment = .center
+        label.makeShadow(color: .black,
+                         opacity: 0.5,
+                         offset: CGSize(width: 0, height: 3),
+                         radius: 3)
+        return label
+    }()
+    private let confirmButton: UIButton = {
+        let button = MainButton()
+        button.title = "확인"
+        return button
+    }()
     
     // MARK: - life cycle
     
@@ -23,6 +44,18 @@ final class OpenManittoPopupViewController: BaseViewController {
             $0.top.equalTo(view.safeAreaLayoutGuide).inset(UIScreen.main.bounds.size.height * 0.15)
             $0.leading.trailing.equalToSuperview().inset(21)
             $0.height.equalTo(popupView.snp.width).multipliedBy(1.16)
+        }
+        
+        view.addSubview(confirmButton)
+        confirmButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(31)
+            $0.centerX.equalToSuperview()
+        }
+        
+        popupView.addSubview(openMentLabel)
+        openMentLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(51)
+            $0.centerX.equalToSuperview()
         }
     }
     
