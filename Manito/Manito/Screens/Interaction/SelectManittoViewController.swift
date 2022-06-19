5d418235fe244c1b626c61d23a464c7a2dbeea60
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-19 12:14:33 +0900

    [ADD] Manitto Random VC 생성(#70)

---
+++ Manito/Manito/Screens/Interaction/SelectManittoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SelectManittoViewController.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 2022/06/19.
+//
+
+import UIKit
+
+final class SelectManittoViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
