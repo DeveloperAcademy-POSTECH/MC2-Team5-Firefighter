d04255b36ff8095e79c4989f14fc09f5c018be3b
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-06-19 11:07:06 +0900

    [ADD] OpenManittoPopupVC 생성(#68)

---
+++ Manito/Manito/Screens/Interaction/OpenManittoPopupViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OpenManittoPopupViewController.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 2022/06/19.
+//
+
+import UIKit
+
+final class OpenManittoPopupViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
