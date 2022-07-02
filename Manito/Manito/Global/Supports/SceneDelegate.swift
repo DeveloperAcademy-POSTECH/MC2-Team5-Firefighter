97aec712c00e08e1ef06ded13c5d85e075862bfc
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-07-02 23:12:51 +0900

    [FEAT] SettingView init commit (#98)

--- Manito/Manito/Global/Supports/SceneDelegate.swift
+++ Manito/Manito/Global/Supports/SceneDelegate.swift
@@ -16,7 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+//        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
+        let mainViewController = SettingViewController() // 맨 처음 보여줄 ViewController
+
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ Manito/Manito/Screens/Setting/SettingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingViewController.swift
+//  Manito
+//
+//  Created by 이성호 on 2022/07/02.
+//
+
+import UIKit
+
+import SnapKit
+
+class SettingViewController: BaseViewController {
+ 
+    // MARK: - Property
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func render() {
+    
+    }
+    
+    // MARK: - Configure
+    
+    override func configUI() {
+        super.configUI()
+    }
+    
+    // MARK: - Selectors
+    // MARK: - Functions
+}
