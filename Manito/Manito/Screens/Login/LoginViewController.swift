9d00c0b60c5c4e124e0244958c124fecd0c5fce5
chemi <wbsldj7645@naver.com>
2022-07-07 19:38:37 +0900

    [ADD] 애플로그인 버튼 추가

--- Manito/Manito/Screens/Login/LoginViewController.swift
+++ Manito/Manito/Screens/Login/LoginViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Mingwan Choi on 2022/07/07.
 //
 
+import AuthenticationServices
 import UIKit
 
 import SnapKit
@@ -20,6 +21,11 @@ class LoginViewController: BaseViewController {
         return view
     }()
     private let logoTextImageView = UIImageView(image: ImageLiterals.imgTextLogo)
+    private let appleLoginButton: ASAuthorizationAppleIDButton = {
+        let button = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
+        button.cornerRadius = 25
+        return button
+    }()
 
     // MARK: - life cycle
 
@@ -34,11 +40,20 @@ class LoginViewController: BaseViewController {
             $0.centerY.equalToSuperview().offset(-92)
             $0.width.height.equalTo(130)
         }
-        
+
         view.addSubview(logoTextImageView)
         logoTextImageView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(logoImageView.snp.bottom).offset(7)
+        }
+
+        appleLoginButton.layer.cornerRadius = 25
+        view.addSubview(appleLoginButton)
+        appleLoginButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(65)
+            $0.height.equalTo(50)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(35)
         }
     }
 }
