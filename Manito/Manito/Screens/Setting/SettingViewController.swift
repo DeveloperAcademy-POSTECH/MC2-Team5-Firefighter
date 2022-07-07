e5c0a2e4d460ffb85b9d785826f19ed73b1e59f6
Hoya <sholee0508@naver.com>
2022-07-07 18:10:59 +0900

    [FEAT] 설정뷰 구현

--- Manito/Manito/Screens/Setting/SettingViewController.swift
+++ Manito/Manito/Screens/Setting/SettingViewController.swift
@@ -26,8 +26,10 @@ class SettingViewController: BaseViewController {
     // MARK: - Property
     
     private lazy var tableView: UITableView = {
-         let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        let tableView = UITableView()
+        tableView.register(SettingViewTableCell.self, forCellReuseIdentifier: SettingViewTableCell.identifier)
+        tableView.separatorStyle = .none
+        tableView.alwaysBounceVertical = false
         return tableView
     }()
 
@@ -38,8 +40,8 @@ class SettingViewController: BaseViewController {
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
-        super.viewDidLoad()
         configureModels()
+        super.viewDidLoad()
         setupDelegate()
     }
     
@@ -47,16 +49,17 @@ class SettingViewController: BaseViewController {
      
         view.addSubview(imageRow)
         imageRow.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.height.equalTo(40)
             $0.centerX.equalToSuperview()
         }
         
         view.addSubview(tableView)
         tableView.snp.makeConstraints {
-            $0.top.equalTo(imageRow.snp.bottom).offset(20)
+            $0.top.equalTo(imageRow.snp.bottom)
             $0.centerX.equalToSuperview()
-            $0.width.equalToSuperview()
-            $0.height.equalTo(500)
+            $0.width.equalToSuperview().inset(Size.leadingTrailingPadding)
+            $0.height.equalTo(sections.count*70)
         }
     }
     
@@ -76,21 +79,65 @@ class SettingViewController: BaseViewController {
     }
     
     private func configureModels() {
-        sections.append(Section(title: "닉네임 변경", options: [Option(title: "닌게임 변경하기", handler: { [weak self] in
+        sections.append(Section(title: "닉네임 변경", options: [Option(title: "닉네임 변경하기", handler: { [weak self] in
             DispatchQueue.main.async {
-                self?.changeNickname()
+                self?.goToChangeNickname()
             }
         })]))
         
-        sections.append(Section(title: "계정 복구", options: [Option(title: "계정 복구", handler: { [weak self] in
+        sections.append(Section(title: "계정 복구", options: [Option(title: "개인정보 처리방침", handler: { [weak self] in
             DispatchQueue.main.async {
-                self?.changeNickname()
+                self?.goToPersonalInfomation()
+            }
+        })]))
+        
+        sections.append(Section(title: "이용 약관", options: [Option(title: "이용 약관", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.goToTermsOfService()
+            }
+        })]))
+        
+        sections.append(Section(title: "개발자 정보", options: [Option(title: "개발자 정보", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.goToDeveloperInfo()
+            }
+        })]))
+        
+        sections.append(Section(title: "문의하기", options: [Option(title: "문의하기", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.goToHelp()
+            }
+        })]))
+        
+        sections.append(Section(title: "로그아웃", options: [Option(title: "로그아웃", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.goToLogOut()
             }
         })]))
     }
     
-    private func changeNickname() {
-        print("ss")
+    private func goToChangeNickname() {
+        print("goToChangeNickname")
+    }
+    
+    private func goToPersonalInfomation() {
+        print("goToPersonalInfomation")
+    }
+    
+    private func goToTermsOfService() {
+        print("goToTermsOfService")
+    }
+    
+    private func goToDeveloperInfo() {
+        print("goToDeveloperInfo")
+    }
+    
+    private func goToHelp() {
+        print("goToHelp")
+    }
+    
+    private func goToLogOut() {
+        print("goToLogOut")
     }
 }
 
@@ -110,8 +157,18 @@ extension SettingViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let model = sections[indexPath.section].options[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell" ,for: indexPath)
+        let model = sections[indexPath.section].options[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: SettingViewTableCell.identifier ,for: indexPath) as! SettingViewTableCell
+        cell.titleLabel.text = model.title
+        cell.selectionStyle = .none
         return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = sections[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
 }
