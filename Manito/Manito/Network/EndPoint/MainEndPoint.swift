ab693438ab4de529aa7cd22b563f78492aa66313
chemi <wbsldj7645@naver.com>
2022-07-10 21:02:22 +0900

    [CHORE] endpoint 초기 설정

---
+++ Manito/Manito/Network/EndPoint/MainEndPoint.swift
@@ -0,0 +1,57 @@
+//
+//  MainEndPoint.swift
+//  Manito
+//
+//  Created by Mingwan Choi on 2022/07/10.
+//
+
+import Foundation
+
+enum MainEndPoint: EndPointable  {
+    case getCommonMission
+    case getManittoList(page: String, perPage: String)
+    case getRoomStateCheck
+
+    var requestTimeOut: Float {
+        return 30
+    }
+
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .getCommonMission:
+            return .get
+        case.getManittoList:
+            return .get
+        case.getRoomStateCheck:
+            return .get
+        }
+    }
+
+    var requestBody: Data? {
+        switch self {
+        case .getCommonMission:
+            return nil
+        case .getManittoList:
+            return nil
+        case .getRoomStateCheck:
+            return nil
+        }
+    }
+
+    func getURL(baseURL: String) -> String {
+        switch self {
+        case .getCommonMission:
+            return {
+                return "\(baseURL)/api/missions/common"
+            }()
+        case .getManittoList(let page, let perPage):
+            return {
+                return "\(baseURL)/api/rooms?page=\(page)&per_page=\(perPage)"
+            }()
+        case .getRoomStateCheck:
+            return {
+                return "\(baseURL)/api/rooms/:roomId/state"
+            }()
+        }
+    }
+}
