71c169ded1e25a646aca0e88e07a48da8a0247e7
chemi <wbsldj7645@naver.com>
2022-07-13 15:06:14 +0900

    [ADD] DetailDoneEndPoint 추가

--- Manito/Manito/Network/EndPoint/DetailDoneEndPoint.swift
+++ Manito/Manito/Network/EndPoint/DetailDoneEndPoint.swift
@@ -7,9 +7,52 @@
 
 import Foundation
 
-enum DetailDoneEndPoint {
+enum DetailDoneEndPoint: EndPointable {
     case getWithFriend(roomId: String)
     case getHistoryWithManitto(roomId: String, subject: String)
-    case getHistoryWithManatte(roomId: String, subject: String)
-    case getDoneRoomInfo(roomId: String)
+    case getHistoryWithManitte(roomId: String, subject: String)
+    case getDoneRoomInfo(roomId: String, subject: String)
+
+    var requestTimeOut: Float {
+        return 20
+    }
+
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .getWithFriend:
+            return .get
+        case .getHistoryWithManitto:
+            return .get
+        case .getHistoryWithManitte:
+            return .get
+        case .getDoneRoomInfo:
+            return .get
+        }
+    }
+
+    var requestBody: Data? {
+        switch self {
+        case .getWithFriend:
+            return nil
+        case .getHistoryWithManitto:
+            return nil
+        case .getHistoryWithManitte:
+            return nil
+        case .getDoneRoomInfo:
+            return nil
+        }
+    }
+
+    func getURL(baseURL: String) -> String {
+        switch self {
+        case .getWithFriend(let roomId):
+            return "\(baseURL)/api/rooms/\(roomId))/participants"
+        case .getHistoryWithManitto(let roomId, let subject):
+            return "\(baseURL)/api/rooms/\(roomId)/memories?subject=\(subject)"
+        case .getHistoryWithManitte(let roomId, let subject):
+            return "\(baseURL)/api/rooms/\(roomId)/memories?subject=\(subject)"
+        case .getDoneRoomInfo(let roomId, let subject):
+            return "\(baseURL)/api/rooms/\(roomId)?state=\(subject)"
+        }
+    }
 }
