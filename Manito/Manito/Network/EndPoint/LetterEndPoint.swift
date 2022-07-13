43a1c0d26d618377cc3f5a364c34ff70a81c4cac
chemi <wbsldj7645@naver.com>
2022-07-13 16:32:41 +0900

    [ADD] LetterEndPoint 추가

--- Manito/Manito/Network/EndPoint/LetterEndPoint.swift
+++ Manito/Manito/Network/EndPoint/LetterEndPoint.swift
@@ -7,9 +7,54 @@
 
 import Foundation
 
-enum LetterEndPoint {
-    case sendLetter(roomId: String)
+enum LetterEndPoint: EndPointable {
+    case sendLetter(roomId: String, dto: SendMessageDTO)
     case getSendLetter(roomId: String)
     case getReceiveLetter(roomId: String)
-    case changeReadMessage(roomId: String)
+    case changeReadMessage(roomId: String, status: String)
+
+    var requestTimeOut: Float {
+        return 20
+    }
+
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .sendLetter:
+            return .post
+        case .getSendLetter:
+            return .get
+        case .getReceiveLetter:
+            return .get
+        case .changeReadMessage:
+            return .patch
+        }
+    }
+
+    var requestBody: Data? {
+        switch self {
+        case .sendLetter(_, let dto):
+            let body = dto
+            return body.encode()
+        case .getSendLetter:
+            return nil
+        case .getReceiveLetter:
+            return nil
+        case .changeReadMessage(_, let status):
+            let body = ["status": status]
+            return body.encode()
+        }
+    }
+
+    func getURL(baseURL: String) -> String {
+        switch self {
+        case .sendLetter(let roomId, _):
+            return "\(baseURL)/api/rooms/\(roomId)/messages"
+        case .getSendLetter(let roomId):
+            return "\(baseURL)/api/rooms/\(roomId)/messages"
+        case .getReceiveLetter(let roomId):
+            return "\(baseURL)/api/rooms/\(roomId)/messages"
+        case .changeReadMessage(let roomId, _):
+            return "\(baseURL)/api/rooms/\(roomId)/messages/status"
+        }
+    }
 }
