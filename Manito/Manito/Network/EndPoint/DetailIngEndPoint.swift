8bb09cd6c15634d7e6a9207f14c4ceb18258b6d4
chemi <wbsldj7645@naver.com>
2022-07-13 14:59:43 +0900

    [ADD] DetailIngEndPoint 추가

--- Manito/Manito/Network/EndPoint/DetailIngEndPoint.swift
+++ Manito/Manito/Network/EndPoint/DetailIngEndPoint.swift
@@ -7,7 +7,38 @@
 
 import Foundation
 
-enum DetailIngEndPoint {
+enum DetailIngEndPoint: EndPointable {
     case getWithFriend(roomId: String)
     case getStartingRoomInfo(roomId: String, state: String)
+
+    var requestTimeOut: Float {
+        return 20
+    }
+
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .getWithFriend:
+            return .get
+        case .getStartingRoomInfo:
+            return .get
+        }
+    }
+
+    var requestBody: Data? {
+        switch self {
+        case .getWithFriend:
+            return nil
+        case .getStartingRoomInfo:
+            return nil
+        }
+    }
+
+    func getURL(baseURL: String) -> String {
+        switch self {
+        case .getWithFriend(let roomId):
+            return "\(baseURL)/api/rooms/\(roomId))/participants"
+        case .getStartingRoomInfo(let roomId, let state):
+            return "\(baseURL)/api/rooms/\(roomId)?state=\(state)"
+        }
+    }
 }
