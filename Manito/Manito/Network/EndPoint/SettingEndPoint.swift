a7c8ee6f3ccae3d219895ff387d28b2a2a30366e
chemi <wbsldj7645@naver.com>
2022-07-13 16:39:19 +0900

    [ADD] SettingEndPoint 추가

--- Manito/Manito/Network/EndPoint/SettingEndPoint.swift
+++ Manito/Manito/Network/EndPoint/SettingEndPoint.swift
@@ -7,6 +7,32 @@
 
 import Foundation
 
-enum SettingEndPoint {
-    case editUserInfo
+enum SettingEndPoint: EndPointable {
+    case editUserInfo(nickName: String)
+
+    var requestTimeOut: Float {
+        return 20
+    }
+
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .editUserInfo:
+            return .put
+        }
+    }
+
+    var requestBody: Data? {
+        switch self {
+        case .editUserInfo(let nickName):
+            let body = ["nickName": nickName]
+            return body.encode()
+        }
+    }
+
+    func getURL(baseURL: String) -> String {
+        switch self {
+        case .editUserInfo(_):
+            return "\(baseURL)/api/user"
+        }
+    }
 }
