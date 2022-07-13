e0db5808ffb9f547ed8c8992a37abafd5ecd32f0
chemi <wbsldj7645@naver.com>
2022-07-13 14:43:08 +0900

    [ADD] DetailWaitEndPoint 추가

--- Manito/Manito/Network/EndPoint/DetailWaitEndPoint.swift
+++ Manito/Manito/Network/EndPoint/DetailWaitEndPoint.swift
@@ -7,10 +7,64 @@
 
 import Foundation
 
-enum DetailWaitEndPoint {
+enum DetailWaitEndPoint: EndPointable {
     case getWithFriend(roomId: String)
     case getWaitingRoomInfo(roomId: String, state: String)
-    case startManitto(roomId: String)
-    case editRoomInfo(roomId: String)
+    case startManitto(roomId: String, state: String)
+    case editRoomInfo(roomId: String, roomInfo: RoomDTO)
     case deleteRoom(roomId: String)
+
+    var requestTimeOut: Float {
+        return 30
+    }
+
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .getWithFriend:
+            return .get
+        case .getWaitingRoomInfo:
+            return .get
+        case .startManitto:
+            return .patch
+        case .editRoomInfo:
+            return .put
+        case .deleteRoom:
+            return .delete
+        }
+    }
+
+    var requestBody: Data? {
+        switch self {
+        case .getWithFriend:
+            return nil
+        case .getWaitingRoomInfo:
+            return nil
+        case .startManitto(_, let state):
+            let parameters = ["state": state]
+            return parameters.encode()
+        case .editRoomInfo(_, let roomInfo):
+            let parameters = ["title": roomInfo.title,
+                "capacity": roomInfo.capacity.description,
+                "startDate": roomInfo.startDate,
+                "endDate": roomInfo.endDate]
+            return parameters.encode()
+        case .deleteRoom:
+            return nil
+        }
+    }
+
+    func getURL(baseURL: String) -> String {
+        switch self {
+        case .getWithFriend(let roomId):
+            return "\(baseURL)/api/rooms/\(roomId))/participants"
+        case .getWaitingRoomInfo(let roomId, let state):
+            return "\(baseURL)/api/rooms/\(roomId)?state=\(state))"
+        case .startManitto(let roomId, _):
+            return "\(baseURL)/api/rooms/\(roomId)/state"
+        case .editRoomInfo(let roomId, _):
+            return "\(baseURL)/api/rooms/\(roomId)"
+        case .deleteRoom(let roomId):
+            return "\(baseURL)/api/rooms/\(roomId)"
+        }
+    }
 }
