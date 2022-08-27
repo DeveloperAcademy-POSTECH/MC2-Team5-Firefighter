034b5d7e241f8c4f7068372e4bae87c2e597906d
chemi <wbsldj7645@naver.com>
2022-08-27 21:11:34 +0900

    [CHORE] API주소, 토큰 설정 (#163)

--- Manito/Manito/Network/Service/APIEnvironment.swift
+++ Manito/Manito/Network/Service/APIEnvironment.swift
@@ -16,13 +16,13 @@ extension APIEnvironment {
     var baseUrl: String {
         switch self {
         case .development:
-            return ""
+            return "http://43.200.81.247:8080/api/v1/"
         case .production:
             return ""
         }
     }
     
     var token: String {
-        return ""
+        return "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b2tlbnRva2VuIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE2NjAyMDI0MjEsImV4cCI6MTY2Nzk3ODQyMX0.T2bf5BA-81Dc_snRFeP9_Z9xnWLptABtQIeT_smIVPo"
     }
 }
