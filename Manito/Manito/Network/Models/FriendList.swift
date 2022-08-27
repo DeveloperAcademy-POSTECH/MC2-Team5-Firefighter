b3f3ab84c4e4db8cc3830c5358446a01d4dd22d3
chemi <wbsldj7645@naver.com>
2022-08-28 00:38:28 +0900

    [ADD] DetailWaitAPI 추가 (#163)

---
+++ Manito/Manito/Network/Models/FriendList.swift
@@ -0,0 +1,8 @@
+//
+//  FriendList.swift
+//  Manito
+//
+//  Created by Mingwan Choi on 2022/08/27.
+//
+
+import Foundation
---
+++ Manito/Manito/Network/Protocol/DetailWaitProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  DetailWaitProtocol.swift
+//  Manito
+//
+//  Created by Mingwan Choi on 2022/08/27.
+//
+
+import Foundation
+
+protocol DetailWaitProtocol {
+    func getWithFriend(roomId: String) async throws -> FriendList?
+    func getWaitingRoomInfo(roomId: String, state: String) async throws -> Room?
+    func startManitto(roomId: String, state: String) async throws -> String?
+    func editRoomInfo(roomId: String, roomInfo: RoomDTO) async throws -> String?
+    func deleteRoom(roomId: String) async throws -> String?
+}
