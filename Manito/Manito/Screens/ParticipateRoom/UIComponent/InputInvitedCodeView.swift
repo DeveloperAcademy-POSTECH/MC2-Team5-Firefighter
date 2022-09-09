f28bb8bb4d358a4ca839804c4f6e5836dd6da14b
chemi <wbsldj7645@naver.com>
2022-09-09 15:42:34 +0900

    [FEAT] 최대 글자 표시 (#212)

--- Manito/Manito/Screens/ParticipateRoom/UIComponent/InputInvitedCodeView.swift
+++ Manito/Manito/Screens/ParticipateRoom/UIComponent/InputInvitedCodeView.swift
@@ -11,14 +11,19 @@ import SnapKit
 
 final class InputInvitedCodeView: UIView {
     
-    // MARK: - Property    
+    private let maxLength = 6
+    
+    var changeNextButtonEnableStatus: ((Bool) -> ())?
+    
+    // MARK: - Property
+    
     lazy var roomCodeTextField: UITextField = {
         let textField = UITextField()
         let attributes = [
             NSAttributedString.Key.font : UIFont.font(.regular, ofSize: 18)
         ]
         textField.backgroundColor = .darkGrey002
-        textField.attributedPlaceholder = NSAttributedString(string: "초대코드 입력", attributes: attributes)
+        textField.attributedPlaceholder = NSAttributedString(string: TextLiteral.inputInvitedCodeViewRoomCodeText, attributes: attributes)
         textField.textAlignment = .center
         textField.makeBorderLayer(color: .white)
         textField.font = .font(.regular, ofSize: 18)
@@ -27,9 +32,16 @@ final class InputInvitedCodeView: UIView {
         return textField
     }()
     
-    var changeNextButtonEnableStatus: ((Bool) -> ())?
+    private lazy var roomsTextLimit : UILabel = {
+        let label = UILabel()
+        label.text = "\(String(describing: roomCodeTextField.text?.count ?? 0))/\(maxLength)"
+        label.font = .font(.regular, ofSize: 20)
+        label.textColor = .grey002
+        return label
+    }()
     
-    // MARK: - Init
+    // MARK: - life cycle
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         render()
@@ -39,18 +51,38 @@ final class InputInvitedCodeView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Config
     private func render() {
         self.addSubview(roomCodeTextField)
         roomCodeTextField.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(60)
         }
+        
+        self.addSubview(roomsTextLimit)
+        roomsTextLimit.snp.makeConstraints {
+            $0.top.equalTo(roomCodeTextField.snp.bottom).offset(10)
+            $0.right.equalToSuperview().inset(Size.leadingTrailingPadding)
+        }
+    }
+    
+    // MARK: - func
+    
+    private func setCounter(count: Int) {
+        roomsTextLimit.text = "\(count)/\(maxLength)"
+        checkMaxLength(textField: roomCodeTextField, maxLength: maxLength)
+    }
+    
+    private func checkMaxLength(textField: UITextField, maxLength: Int) {
+        if (textField.text?.count ?? 0 > maxLength) {
+            textField.deleteBackward()
+        }
     }
 }
 
 extension InputInvitedCodeView: UITextFieldDelegate {
-    func textFieldDidChangeSelection(_ textField: UITextField) {        
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        setCounter(count: textField.text?.count ?? 0)
+        
         let hasText = roomCodeTextField.hasText
         changeNextButtonEnableStatus?(hasText)
     }
