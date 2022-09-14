71336157b57e09c38e7df5aa02921bb90dafa6a9
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-09-14 17:41:02 +0900

    [CHORE] request 코드 정리(#245)

--- Manito/Manito/Network/Service/APIService.swift
+++ Manito/Manito/Network/Service/APIService.swift
@@ -12,29 +12,54 @@ final class APIService: Requestable {
     var requestTimeOut: Float = 30
 
     func request<T: Decodable>(_ request: NetworkRequest) async throws -> T? {
-        let sessionConfig = URLSessionConfiguration.default
-        sessionConfig.timeoutIntervalForRequest = TimeInterval(request.requestTimeOut ?? requestTimeOut)
-        guard let encodedUrl = request.url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-            let url = URL(string: encodedUrl) else {
-            throw NetworkError.encodingError
-        }
-        print("encodedUrl = \(encodedUrl)")
-
-        let (data, response) = try await URLSession.shared.data(for: request.buildURLRequest(with: url))
-        guard let httpResponse = response as? HTTPURLResponse else { throw NetworkError.serverError }
+        let (data, httpResponse) = try await requestDataToUrl(request)
+        
         switch httpResponse.statusCode {
         case (200..<300):
             let decoder = JSONDecoder()
             let baseModelData: T? = try decoder.decode(T.self, from: data)
             return baseModelData
         case (300..<500):
-            throw NetworkError.clientError(message: "\(httpResponse.statusCode)")
+            throw NetworkError.clientError(message: httpResponse.statusCode.description)
         default:
             throw NetworkError.serverError
         }
     }
     
     func request<T: Decodable>(_ request: NetworkRequest) async throws -> (T?, Int) {
+        let (data, httpResponse) = try await requestDataToUrl(request)
+        
+        switch httpResponse.statusCode {
+        case (200..<300):
+            let decoder = JSONDecoder()
+            let baseModelData: T? = try decoder.decode(T.self, from: data)
+            return (baseModelData, httpResponse.statusCode)
+        case (300..<500):
+            throw NetworkError.clientError(message: httpResponse.statusCode.description)
+        default:
+            throw NetworkError.serverError
+        }
+    }
+    
+    func requestCreateRoom(_ request: NetworkRequest) async throws -> Int? {
+        let (_, httpResponse) = try await requestDataToUrl(request)
+        
+        switch httpResponse.statusCode {
+        case (200..<300):
+            let responseUrl = httpResponse.value(forHTTPHeaderField: "Location")
+            return responseUrl?.roomId
+        case (300..<500):
+            throw NetworkError.clientError(message: httpResponse.statusCode.description)
+        default:
+            throw NetworkError.serverError
+        }
+    }
+}
+
+extension APIService {
+    typealias UrlResponse = (Data, HTTPURLResponse)
+    
+    private func requestDataToUrl(_ request: NetworkRequest) async throws -> UrlResponse {
         let sessionConfig = URLSessionConfiguration.default
         sessionConfig.timeoutIntervalForRequest = TimeInterval(request.requestTimeOut ?? requestTimeOut)
         guard let encodedUrl = request.url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
@@ -45,38 +70,8 @@ final class APIService: Requestable {
 
         let (data, response) = try await URLSession.shared.data(for: request.buildURLRequest(with: url))
         guard let httpResponse = response as? HTTPURLResponse else { throw NetworkError.serverError }
-        switch httpResponse.statusCode {
-        case (200..<300):
-            let decoder = JSONDecoder()
-            let baseModelData: T? = try decoder.decode(T.self, from: data)
-            return (baseModelData, httpResponse.statusCode)
-        case (300..<500):
-            throw NetworkError.clientError(message: "\(httpResponse.statusCode)")
-        default:
-            throw NetworkError.serverError
-        }
-    }
-    
-    func requestCreateRoom(_ request: NetworkRequest) async throws -> Int? {
-        let sessionConfig = URLSessionConfiguration.default
-        sessionConfig.timeoutIntervalForRequest = TimeInterval(request.requestTimeOut ?? requestTimeOut)
-        guard let encodedUrl = request.url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-            let url = URL(string: encodedUrl) else {
-            throw NetworkError.encodingError
-        }
-        print("encodedUrl = \(encodedUrl)")
-
-        let (_, response) = try await URLSession.shared.data(for: request.buildURLRequest(with: url))
-        guard let httpResponse = response as? HTTPURLResponse else { throw NetworkError.serverError }
-        switch httpResponse.statusCode {
-        case (200..<300):
-            let responseUrl = httpResponse.value(forHTTPHeaderField: "Location")
-            return responseUrl?.roomId
-        case (300..<500):
-            throw NetworkError.clientError(message: "\(httpResponse.statusCode)")
-        default:
-            throw NetworkError.serverError
-        }
+        
+        return (data, httpResponse)
     }
 }
 
