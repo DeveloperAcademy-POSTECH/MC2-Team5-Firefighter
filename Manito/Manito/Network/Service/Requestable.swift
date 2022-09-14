41f804e30791c7fe0f5fafd1807acb73aca40360
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-09-14 16:07:15 +0900

    [ADD] request 코드 작성(#245)

--- Manito/Manito/Network/Service/Requestable.swift
+++ Manito/Manito/Network/Service/Requestable.swift
@@ -11,4 +11,6 @@ protocol Requestable {
     var requestTimeOut: Float { get }
     
     func request<T: Decodable>(_ request: NetworkRequest) async throws -> T?
+    func request<T: Decodable>(_ request: NetworkRequest) async throws -> (T?, Int)
+    func requestCreateRoom(_ request: NetworkRequest) async throws -> Int?
 }
