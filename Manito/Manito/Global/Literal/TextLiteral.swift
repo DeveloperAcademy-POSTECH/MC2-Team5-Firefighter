468dea2e2d7eb3f856f0dfc5394ab5e5dbc47294
chemi <wbsldj7645@naver.com>
2022-09-14 23:50:35 +0900

    [CHORE] TextLiteral 수정 (#259)

--- Manito/Manito/Global/Literal/TextLiteral.swift
+++ Manito/Manito/Global/Literal/TextLiteral.swift
@@ -62,7 +62,7 @@ enum TextLiteral {
     // MARK: - MainViewController
     static let mainViewControllerMenuTitle: String = "참여중인 애니또"
     static let mainViewControllerGuideTitle: String = "공통 미션이란?"
-    static let mainViewControllerGuideDescription: String = "마니또에 참여한 모두에게 \n수행하는 미션이에요. "
+    static let mainViewControllerGuideDescription: String = "공통 미션이란?\n마니또에 참여한 모두에게 \n수행하는 미션이에요. "
     static let mainViewControllerNewRoomAlert: String = "새로운 마니또 시작"
 
     // MARK: - OpenManittoViewController
@@ -123,7 +123,7 @@ enum TextLiteral {
     // MARK: - DetailIngViewController
     static let detailIngViewControllerManitoOpenButton: String = "마니또 공개"
     static let detailIngViewControllerGuideTitle: String = "개별 미션이란?"
-    static let detailIngViewControllerText: String = "나의 마니띠에게\n수행하는 미션이에요."
+    static let detailIngViewControllerText: String = "개별 미션이란?\n나의 마니띠에게\n수행하는 미션이에요."
     static let detailIngViewControllerDoneMissionText: String = "종료된 마니또예요"
     
     // MARK: - CalendarView
