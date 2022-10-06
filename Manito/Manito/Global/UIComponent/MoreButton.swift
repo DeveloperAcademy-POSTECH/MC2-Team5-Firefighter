daebdfb24792d93165eb5c5d7e82bdcfff6f8cbe
chemi <wbsldj7645@naver.com>
2022-10-06 21:30:39 +0900

    [ADD] moreButton 추가 (#311)

---
+++ Manito/Manito/Global/UIComponent/MoreButton.swift
@@ -0,0 +1,24 @@
+//
+//  MoreButton.swift
+//  Manito
+//
+//  Created by Mingwan Choi on 2022/10/06.
+//
+
+import UIKit
+
+final class MoreButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: .init(origin: .zero, size: .init(width: 44, height: 44)))
+        configUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configUI() {
+        self.setImage(ImageLiterals.icMore, for: .normal)
+        self.tintColor = .white
+    }
+}
