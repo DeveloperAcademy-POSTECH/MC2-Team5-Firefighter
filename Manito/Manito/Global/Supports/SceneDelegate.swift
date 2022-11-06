e9691a792dd1f57e7f3531c597abae685612be7a
Dinner <junghwan3588@gm.gist.ac.kr>
2022-11-06 15:51:30 +0900

    [CHORE] 코드 리뷰 반영 (#325)

--- Manito/Manito/Global/Supports/SceneDelegate.swift
+++ Manito/Manito/Global/Supports/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let storyboard = UIStoryboard(name: "Splash", bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: SplashViewController.className) as? SplashViewController else { return }
         
         window.rootViewController = viewController
         self.window = window
