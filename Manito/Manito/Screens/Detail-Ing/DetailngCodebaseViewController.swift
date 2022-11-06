daf8eb470e3408c493162b544df4acdc8210631e
Dinner <junghwan3588@gm.gist.ac.kr>
2022-11-06 12:02:02 +0900

    [CHORE] 코드 리뷰 반영 (#325)

--- Manito/Manito/Screens/Detail-Ing/DetailngCodebaseViewController.swift
+++ Manito/Manito/Screens/Detail-Ing/DetailngCodebaseViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 import SnapKit
 
-class DetailingCodebaseViewController: BaseViewController {
+final class DetailingCodebaseViewController: BaseViewController {
 
     // MARK: - property
     
