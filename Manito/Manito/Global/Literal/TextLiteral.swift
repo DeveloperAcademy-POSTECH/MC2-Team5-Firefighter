580b8f4f8b84779097efbacf8c27502e1747e539
chemi <wbsldj7645@naver.com>
2022-11-08 15:35:25 +0900

    [CHORE] 완료된 방 방장 삭제 Writing 추가 (#336)

--- Manito/Manito/Global/Literal/TextLiteral.swift
+++ Manito/Manito/Global/Literal/TextLiteral.swift
@@ -134,8 +134,9 @@ enum TextLiteral {
     static let detailIngViewControllerText: String = "개별 미션이란?\n나의 마니띠에게\n수행하는 미션이에요."
     static let detailIngViewControllerDoneMissionText: String = "종료된 마니또예요"
     static let detailIngViewControllerDoneExitAlertTitle: String = "정말 방을 나가시겠습니까?"
+    static let detailIngViewControllerDoneExitAlertAdminTitle: String = "정말 방을 삭제하시겠습니까?"
     static let detailIngViewControllerDoneExitAlertMessage: String = "나간 방은 다시 들어올 수 없어요"
-    static let detailIngViewControllerDoneExitAlertAdmin: String = "방장은 방을 나갈 수 없어요"
+    static let detailIngViewControllerDoneExitAlertAdmin: String = "방장이 방을 삭제하면\n 참여자들의 방도 삭제됩니다"
     
     // MARK: - CalendarView
     static let calendarViewAlertMaxTitle: String = "최대 선택 기간을 넘었어요"
