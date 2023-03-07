b9434a11c29fa12d78d352aef78e1b8c5b010b19
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-03-07 15:38:01 +0900

    [ADD] LetterView 생성(#399)

---
+++ Manito/Manito/Screens/Letter/View/LetterView.swift
@@ -0,0 +1,37 @@
+//
+//  LetterView.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 2023/03/07.
+//
+
+import UIKit
+
+import SnapKit
+
+final class LetterView: UIView {
+
+    // MARK: - ui component
+
+
+    // MARK: - property
+
+
+    // MARK: - init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - func
+
+    private func setupLayout() {
+
+    }
+}
