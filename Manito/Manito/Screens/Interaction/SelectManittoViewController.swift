265a9ba03bcfb24ccffd9fd88ee981f6e3368887
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-03-26 19:03:25 +0900

    [CHORE] Information Label configuration 수정(#428)

--- Manito/Manito/Screens/Interaction/SelectManittoViewController.swift
+++ Manito/Manito/Screens/Interaction/SelectManittoViewController.swift
@@ -20,7 +20,10 @@ final class SelectManittoViewController: BaseViewController {
     private let informationLabel: UILabel = {
         let label = UILabel()
         label.font = .font(.regular, ofSize: 20)
+        label.numberOfLines = 2
         label.text = TextLiteral.selectManittoViewControllerInformationText
+        label.addLabelSpacing()
+        label.textAlignment = .center
         return label
     }()
     private let nameLabel: UILabel = {
