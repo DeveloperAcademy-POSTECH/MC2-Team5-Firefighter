7b2444226a9433e73bbb7b90832800073cbce40f
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-05-10 16:15:44 +0900

    [REFACTOR] ChooseCharacterView 생성 (#453)

---
+++ Manito/Manito/Screens/CreateRoom/UIComponent/ChooseCharacterView.swift
@@ -0,0 +1,100 @@
+//
+//  ChooseCharacterView.swift
+//  Manito
+//
+//  Created by 이성호 on 2023/05/10.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ChooseCharacterView: UIView {
+    
+    private enum InternalSize {
+        static let collectionHorizontalSpacing: CGFloat = 29.0
+        static let cellInterSpacing: CGFloat = 39.0
+        static let cellLineSpacing: CGFloat = 24.0
+        static let cellWidth: CGFloat = (UIScreen.main.bounds.size.width - (collectionHorizontalSpacing * 2 + cellInterSpacing * 2)) / 3
+        static let collectionInset = UIEdgeInsets(top: 0,
+                                                  left: 0,
+                                                  bottom: 0,
+                                                  right: 0)
+    }
+    
+    // MARK: - ui component
+
+    private let collectionViewFlowLayout: UICollectionViewFlowLayout = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.sectionInset = InternalSize.collectionInset
+        flowLayout.minimumLineSpacing = InternalSize.cellLineSpacing
+        flowLayout.minimumInteritemSpacing = InternalSize.cellInterSpacing
+        flowLayout.sectionHeadersPinToVisibleBounds = true
+        flowLayout.itemSize = CGSize(width: InternalSize.cellWidth, height: InternalSize.cellWidth)
+        return flowLayout
+    }()
+    private lazy var manittoCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+        collectionView.backgroundColor = .clear
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.isScrollEnabled = false
+        collectionView.register(cell: CharacterCollectionViewCell.self,
+                                forCellWithReuseIdentifier: CharacterCollectionViewCell.className)
+        return collectionView
+    }()
+    
+    // MARK: - property
+    
+    private var characterIndex: Int = 0
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - func
+    
+    private func setupLayout() {
+        self.addSubview(self.manittoCollectionView)
+        self.manittoCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension ChooseCharacterView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Character.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell: CharacterCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as? CharacterCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.characterBackground = Character.allCases[indexPath.item].color
+        cell.characterImageView.image = Character.allCases[indexPath.item].image
+        cell.setImageBackgroundColor()
+        
+        if indexPath.item == 0 {
+            cell.isSelected = true
+            collectionView.selectItem(at: indexPath, animated: false , scrollPosition: .init())
+        }
+        
+        return cell
+    }
+}
+
+extension ChooseCharacterView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.characterIndex = indexPath.item
+    }
+}
