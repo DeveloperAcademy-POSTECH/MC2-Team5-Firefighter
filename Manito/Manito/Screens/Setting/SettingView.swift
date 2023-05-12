bd1ee5c84f8832e5a233dfa9438ac9d5eaefca45
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-05-12 11:18:01 +0900

    [ADD] SettingView 생성 (#458)

---
+++ Manito/Manito/Screens/Setting/SettingView.swift
@@ -0,0 +1,27 @@
+//
+//  SettingView.swift
+//  Manito
+//
+//  Created by 이성호 on 2023/05/12.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SettingView: UIView {
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - func
+    
+}
