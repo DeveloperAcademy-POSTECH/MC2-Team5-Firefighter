a2c40c3001fc27af95f8a098d281a88cd9b6fcf6
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-06-21 00:01:18 +0900

    [FEAT] ViewModel transform 코드 구성 완료(#465)

--- Manito/Manito/Screens/Letter/ViewModels/LetterViewModel.swift
+++ Manito/Manito/Screens/Letter/ViewModels/LetterViewModel.swift
@@ -10,19 +10,28 @@ import Foundation
 
 final class LetterViewModel: ViewModelType {
 
+    enum MessageType: Int {
+        case send = 0
+        case received = 1
+    }
+
     struct Input {
-        let viewDidLoad: AnyPublisher<Void, Never>
-        let segmentControlValueChanged: CurrentValueSubject<Int, Never>
+        let viewDidLoad: AnyPublisher<MessageType, Never>
+        let segmentControlValueChanged: CurrentValueSubject<MessageType, Never>
         let refresh: PassthroughSubject<Void, Never>
     }
 
     struct Output {
         let messages: PassthroughSubject<[Message], Never>
+        let index: PassthroughSubject<Int, Never>
     }
 
     // MARK: - property
 
     private let messageSubject: PassthroughSubject<[Message], Never> = PassthroughSubject()
+    private let indexSubject: PassthroughSubject<Int, Never> = PassthroughSubject()
+
+    private var cancelBag: Set<AnyCancellable> = Set()
 
     private let service: LetterServicable
 
@@ -35,12 +44,43 @@ final class LetterViewModel: ViewModelType {
     // MARK: - Public - func
 
     func transform(from input: Input) -> Output {
-        return Output(messages: self.messageSubject)
+        let refreshWithType = input.refresh
+            .map { MessageType.send }
+
+        Publishers.Merge3(input.viewDidLoad, input.segmentControlValueChanged, refreshWithType)
+            .sink(receiveValue: { [weak self] type in
+                self?.fetchMessages(with: type)
+                self?.sendCurrentIndex(at: type)
+            })
+            .store(in: &self.cancelBag)
+
+        return Output(messages: self.messageSubject, index: self.indexSubject)
     }
 
     // MARK: - Private - func
 
-    private func fetchSendLetter(roomId: String) {
+    private func sendCurrentIndex(at type: MessageType) {
+        let currentIndex = self.currentIndex(at: type)
+        self.indexSubject.send(currentIndex)
+    }
+
+    private func fetchMessages(with type: MessageType) {
+        let roomId = "" // TODO: - roomId 넣기
+
+        switch type {
+        case .send: self.fetchSendMessages(roomId: roomId)
+        case .received: self.fetchReceivedMessages(roomId: roomId)
+        }
+    }
+}
+
+// MARK: - Helper
+extension LetterViewModel {
+    private func currentIndex(at type: MessageType) -> Int {
+        return type.rawValue
+    }
+
+    private func fetchSendMessages(roomId: String) {
         Task {
             do {
                 let messages = try await self.service.fetchSendLetter(roomId: roomId)
@@ -51,7 +91,7 @@ final class LetterViewModel: ViewModelType {
         }
     }
 
-    private func fetchReceivedLetter(roomId: String) {
+    private func fetchReceivedMessages(roomId: String) {
         Task {
             do {
                 let messages = try await self.service.fetchReceiveLetter(roomId: roomId)
