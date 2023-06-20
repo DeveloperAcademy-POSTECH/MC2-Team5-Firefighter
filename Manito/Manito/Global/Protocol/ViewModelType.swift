bca99bb4d9a200af1a8a003792aace4b9cf8e6d8
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-06-20 21:51:25 +0900

    [ADD] ViewModelType과 ViewModel 생성(#465)

---
+++ Manito/Manito/Global/Protocol/ViewModelType.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModelType.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 2023/06/20.
+//
+
+import Foundation
+
+protocol ViewModelType {
+    associatedtype Input
+    associatedtype Output
+
+    func transform(from input: Input) -> Output
+}
--- Manito/Manito/Screens/Letter/ViewModels/LetterViewModel.swift
+++ Manito/Manito/Screens/Letter/ViewModels/LetterViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by SHIN YOON AH on 2023/06/20.
 //
 
+import Combine
 import Foundation
+
+final class LetterViewModel: ViewModelType {
+
+    struct Input {
+
+    }
+
+    struct Output {
+
+    }
+
+    // MARK: - Public - func
+
+    func transform(from input: Input) -> Output {
+        return Output()
+    }
+
+    // MARK: - Private - func
+
+}
