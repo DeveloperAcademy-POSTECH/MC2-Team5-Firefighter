49a075f53ab6651309091ca1e8b60a4178a9e838
chemi <wbsldj7645@naver.com>
2023-06-25 20:08:04 +0900

    [ADD] 미션 수정 네트워크 코드 추가 (#467)

---
+++ Manito/Manito/Network/Protocol/MisstionEditProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  MisstionEditProtocol.swift
+//  Manito
+//
+//  Created by Mingwan Choi on 2023/06/25.
+//
+
+import Foundation
+
+protocol MissionEditProtocol {
+    func patchEditMission(roomId: String, body: MissionDTO) async throws -> String? 
+}
