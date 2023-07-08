09831394bbd99cb76eac614e108b3a51610bfe40
chemi <wbsldj7645@naver.com>
2023-07-08 19:15:47 +0900

    [CHORE] 초대코드 transfer 테스트 코드 추가 (#185)

--- Manito/ManitoTests/DetailWaitViewModelTest.swift
+++ Manito/ManitoTests/DetailWaitViewModelTest.swift
@@ -13,13 +13,28 @@ import XCTest
 final class DetailWaitViewModelTest: XCTestCase {
     
     private let viewModel = DetailWaitViewModel(roomIndex: 1, detailWaitService: DetailWaitService(api: DetailWaitAPI(apiService: APIService())))
-    let testMockRoom = Room.testRoom
     private var cancellable = Set<AnyCancellable>()
+    private let testViewDidLoadSubject = PassthroughSubject<Void, Never>()
+    private let testCodeCopyButtonDidTapSubject = PassthroughSubject<Void, Never>()
+    private let testStartButtonDidTapSubject = PassthroughSubject<Void, Never>()
+    private let testEditMenuButtonDidTapSubject = PassthroughSubject<Void, Never>()
+    private let testDeleteMenuButtonDidTapSubject = PassthroughSubject<Void, Never>()
+    private let testLeaveMenuButtonDidTapSubject = PassthroughSubject<Void, Never>()
+    private let testChangeButtonDidTapSubject = PassthroughSubject<Void, Never>()
+    private var output: DetailWaitViewModel.Output!
     
     override func setUp() {
         super.setUp()
         self.viewModel.setRoomInformation(room: Room.testRoom)
-        self.cancellable = Set<AnyCancellable>()
+        let input = DetailWaitViewModel.Input(
+            viewDidLoad: testViewDidLoadSubject.eraseToAnyPublisher(),
+            codeCopyButtonDidTap: testCodeCopyButtonDidTapSubject.eraseToAnyPublisher(),
+            startButtonDidTap: testStartButtonDidTapSubject.eraseToAnyPublisher(),
+            editMenuButtonDidTap: testEditMenuButtonDidTapSubject.eraseToAnyPublisher(),
+            deleteMenuButtonDidTap: testDeleteMenuButtonDidTapSubject.eraseToAnyPublisher(),
+            leaveMenuButtonDidTap: testLeaveMenuButtonDidTapSubject.eraseToAnyPublisher(),
+            changeButtonDidTap: testChangeButtonDidTapSubject.eraseToAnyPublisher())
+        self.output = self.viewModel.transform(input)
     }
     
     func testMakeRoomInformation() {
@@ -35,36 +50,16 @@ final class DetailWaitViewModelTest: XCTestCase {
         XCTAssertEqual(testRoom.roomInformation?.title, room.roomInformation?.title)
     }
     
-    func testInputOutput() {
+    func testTransferInvitationCode() {
         // given
-        let testViewDidLoadSubject = PassthroughSubject<Void, Never>()
-        let testCodeCopyButtonDidTapSubject = PassthroughSubject<Void, Never>()
-        let testStartButtonDidTapSubject = PassthroughSubject<Void, Never>()
-        let testEditMenuButtonDidTapSubject = PassthroughSubject<Void, Never>()
-        let testDeleteMenuButtonDidTapSubject = PassthroughSubject<Void, Never>()
-        let testLeaveMenuButtonDidTapSubject = PassthroughSubject<Void, Never>()
-        let testChangeButtonDidTapSubject = PassthroughSubject<Void, Never>()
-        
-        let input = DetailWaitViewModel.Input(
-            viewDidLoad: testViewDidLoadSubject.eraseToAnyPublisher(),
-            codeCopyButtonDidTap: testCodeCopyButtonDidTapSubject.eraseToAnyPublisher(),
-            startButtonDidTap: testStartButtonDidTapSubject.eraseToAnyPublisher(),
-            editMenuButtonDidTap: testEditMenuButtonDidTapSubject.eraseToAnyPublisher(),
-            deleteMenuButtonDidTap: testDeleteMenuButtonDidTapSubject.eraseToAnyPublisher(),
-            leaveMenuButtonDidTap: testLeaveMenuButtonDidTapSubject.eraseToAnyPublisher(),
-            changeButtonDidTap: testChangeButtonDidTapSubject.eraseToAnyPublisher())
-        
-        var outputCode: String = ""
+        let checkCode = "ABCDEF"
         // when
-        self.viewModel.setRoomInformation(room: testMockRoom)
-        let output = self.viewModel.transform(input)
-        testCodeCopyButtonDidTapSubject.send(())
         output.code
             .sink { code in
-                outputCode = code
+                XCTAssertEqual(code, checkCode)
             }
             .store(in: &self.cancellable)
         // then
-        XCTAssertEqual(outputCode, "ABCDEF")
+        testCodeCopyButtonDidTapSubject.send(())
     }
 }
