b4dd0428ed91871dc6de779eadef37abc90e704c
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-07-25 22:14:12 +0900

    [CHORE] ViewModel를 asyncMap으로 수정(#475)

--- Manito/Manito/Global/Extension/Publisher+Extension.swift
+++ Manito/Manito/Global/Extension/Publisher+Extension.swift
@@ -36,4 +36,21 @@ extension Publisher {
             }
         }
     }
+
+    func asyncMap<T>(
+        _ transform: @escaping (Output) async throws -> T)
+    -> Publishers.FlatMap<Future<T, Error>, Publishers.SetFailureType<Self, Error>> {
+        flatMap { value in
+            Future { promise in
+                Task {
+                    do {
+                        let output = try await transform(value)
+                        promise(.success(output))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            }
+        }
+    }
 }
