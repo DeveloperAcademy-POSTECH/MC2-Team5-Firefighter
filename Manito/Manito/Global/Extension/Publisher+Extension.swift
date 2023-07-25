452fba2a27bb230983cc0dd17526a14c4b65d1b7
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-07-25 21:44:44 +0900

    [FEAT] Publisher Extension 추가(#475)

---
+++ Manito/Manito/Global/Extension/Publisher+Extension.swift
@@ -0,0 +1,39 @@
+//
+//  Publisher+Extension.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 2023/07/25.
+//
+// https://github.com/boostcampwm-2022/iOS09-burstcamp/blob/develop/burstcamp/burstcamp/Util/Extension/AsyncCompatible/Publisher%2BAsync.swift
+
+import Combine
+
+extension Publisher {
+    func asyncMap<T>(_ transform: @escaping (Output) async -> T)
+    -> Publishers.FlatMap<Future<T, Never>, Self> {
+        self.flatMap { value in
+            Future { promise in
+                Task {
+                    let output = await transform(value)
+                    promise(.success(output))
+                }
+            }
+        }
+    }
+
+    func asyncMap<T>(_ transform: @escaping (Output) async throws -> T)
+    -> Publishers.FlatMap<Future<T, Error>, Self> {
+        self.flatMap { value in
+            Future { promise in
+                Task {
+                    do {
+                        let output = try await transform(value)
+                        promise(.success(output))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            }
+        }
+    }
+}
