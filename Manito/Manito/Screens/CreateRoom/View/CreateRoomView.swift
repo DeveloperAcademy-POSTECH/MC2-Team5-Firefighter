90f11f9d0909b60c7b240c5436aa8bea0e45ae9c
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-08-02 14:39:55 +0900

    [ADD] CreateRoomView 생성 (#403)

--- Manito/Manito/Screens/CreateRoom/View/CreateRoomView.swift
+++ Manito/Manito/Screens/CreateRoom/View/CreateRoomView.swift
@@ -5,4 +5,31 @@
 //  Created by 이성호 on 2023/08/02.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class CreateRoomView: UIView {
+    // MARK: - ui component
+    
+    // MARK: - property
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - life cycle
+    
+    // MARK: - override
+    
+    // MARK: - func
+    
+    // MARK: - selector
+}
