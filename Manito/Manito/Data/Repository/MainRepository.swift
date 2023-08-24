432c80b804208da1a55b16578607b9650ea5c76e
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-08-24 18:43:38 +0900

    [CHORE] Main 부분 DTO 수정(#490)

--- Manito/Manito/Data/Repository/MainRepository.swift
+++ Manito/Manito/Data/Repository/MainRepository.swift
@@ -10,21 +10,21 @@ import Foundation
 import MTNetwork
 
 protocol MainRepository {
-    func fetchCommonMission() async throws -> DailyMission?
-    func fetchManittoList() async throws -> ParticipatingRooms?
+    func fetchCommonMission() async throws -> DailyMissionDTO?
+    func fetchManittoList() async throws -> RoomListDTO?
 }
 
 final class MainRepositoryImpl: MainRepository {
 
     private var provider = Provider<MainEndPoint>()
 
-    func fetchCommonMission() async throws -> DailyMission? {
+    func fetchCommonMission() async throws -> DailyMissionDTO? {
         let response = try await self.provider
             .request(.fetchCommonMission)
         return try response.decode()
     }
 
-    func fetchManittoList() async throws -> ParticipatingRooms? {
+    func fetchManittoList() async throws -> RoomListDTO? {
         let response = try await self.provider
             .request(.fetchManittoList)
         return try response.decode()
