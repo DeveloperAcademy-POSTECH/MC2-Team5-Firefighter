70462620b7f6d7bcb513980c443751ba56c315dc
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-08-29 13:33:40 +0900

    [ADD] DTO 상세 정보 추가(#499)

--- Manito/Manito/Data/DTO/Response/MessageListItemDTO.swift
+++ Manito/Manito/Data/DTO/Response/MessageListItemDTO.swift
@@ -11,18 +11,17 @@ import Foundation
 /// 쪽지, 함께 했던 기억 화면에서 쪽지 내용에 대한 정보들을 반환하는
 /// 데이터 모델 DTO입니다.
 ///
-/// - id: 쪽지의 id 정보
-/// - content: 쪽지의 내용(옵션)
-/// - imageUrl: 쪽지에 포함된 사진(옵션)
-/// - createdDate: 쪽지를 보낸 날짜
-/// - missionInfo: 쪽지에 해당하는 미션(옵션 - 미션을 추가하지 않았던 v1 API)
-///
 
 struct MessageListItemDTO: Decodable {
+    /// 쪽지의 id 정보
     let id: Int?
+    /// 쪽지의 내용(옵션)
     let content: String?
+    /// 쪽지에 포함된 사진(옵션)
     let imageUrl: String?
+    /// 쪽지를 보낸 날짜
     let createdDate: String?
+    /// 쪽지에 해당하는 미션(옵션 - 미션을 추가하지 않았던 v1 API)
     let missionInfo: IndividualMissionDTO?
 }
 
