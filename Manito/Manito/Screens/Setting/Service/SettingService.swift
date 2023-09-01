29cbc8644441375def0a6e60495f456d23f13d7f
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-01 16:26:08 +0900

    [CHORE] 폴더구조 정리 (#505)

---
+++ Manito/Manito/Screens/Setting/Service/SettingService.swift
@@ -0,0 +1,8 @@
+//
+//  SettingService.swift
+//  Manito
+//
+//  Created by 이성호 on 2023/09/01.
+//
+
+import Foundation
---
+++ Manito/Manito/Screens/Setting/ViewModel/SettingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingViewModel.swift
+//  Manito
+//
+//  Created by 이성호 on 2023/09/01.
+//
+
+import Foundation
