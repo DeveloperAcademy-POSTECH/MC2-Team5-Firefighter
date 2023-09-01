14ee23012933e3d30b30634e184050556820aa18
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-02 00:54:17 +0900

    [FEAT] NicknameService 생성 (#507)

--- Manito/Manito/Screens/CreateNickName/Service/NicknameService.swift
+++ Manito/Manito/Screens/CreateNickName/Service/NicknameService.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+protocol NicknameServicable {
+    func putUserInfo(nickname: NicknameDTO) async throws -> NicknameDTO
+}
+
+final class NicknameService: NicknameServicable {
+    
+    private let repository: SettingRepository
+    
+    init(repository: SettingRepository) {
+        self.repository = repository
+    }
+    
+    func putUserInfo(nickname: NicknameDTO) async throws -> NicknameDTO {
+        do {
+            let data = try await self.repository.putUserInfo(nickname: nickname)
+            return data
+        } catch NetworkError.serverError {
+            throw NetworkError.serverError
+        } catch NetworkError.clientError(let message) {
+            throw NetworkError.clientError(message: message)
+        }
+    }
+}
