6a433af0e4ca8cf68e000543dda04cef51bbdc46
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-02 00:06:44 +0900

    [FEAT] NicknameView 생성 (#507)

---
+++ Manito/Manito/Screens/CreateNickName/NicknameView.swift
@@ -0,0 +1,23 @@
+//
+//  NicknameView.swift
+//  Manito
+//
+//  Created by 이성호 on 2023/09/02.
+//
+
+import UIKit
+
+import SnapKit
+
+final class NicknameView: UIView {
+    
+    // MARK: - ui components
+    
+    // MARK: - property
+    
+    // MARK: - init
+    
+    // MARK: - func
+    
+    // MARK: - selector
+}
