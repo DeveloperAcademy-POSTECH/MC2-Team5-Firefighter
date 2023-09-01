0f3f908ccc59e212561aa7f87dd932e065f12192
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-02 00:46:13 +0900

    [FEAT] NicknameViewController View 분리 (#507)

--- Manito/Manito/Screens/CreateNickName/CreateNickNameViewController.swift
+++ Manito/Manito/Screens/CreateNickName/CreateNickNameViewController.swift
@@ -12,51 +12,10 @@ import SnapKit
 class CreateNickNameViewController: BaseViewController {
     
     private let settingRepository: SettingRepository = SettingRepositoryImpl()
+    private lazy var nicknameView: NicknameView = NicknameView(title: TextLiteral.createNickNameViewControllerTitle)
     
-    private var nickname: String = ""
-    private let maxLength = 5
+    // MARK: - property
     
-    // MARK: - Property
-    
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = TextLiteral.createNickNameViewControllerTitle
-        label.font = .font(.regular, ofSize: 34)
-        return label
-    }()
-    private let roomsNameTextField: UITextField = {
-        let textField = UITextField()
-        let attributes = [
-            NSAttributedString.Key.font : UIFont.font(.regular, ofSize: 18)
-        ]
-        textField.backgroundColor = .darkGrey002
-        textField.attributedPlaceholder = NSAttributedString(string: TextLiteral.createNickNameViewControllerAskNickName, attributes:attributes)
-        textField.font = .font(.regular, ofSize: 18)
-        textField.layer.cornerRadius = 10
-        textField.layer.masksToBounds = true
-        textField.layer.borderWidth = 1
-        textField.layer.borderColor = UIColor.white.cgColor
-        textField.textAlignment = .center
-        textField.returnKeyType = .done
-        textField.autocorrectionType = .no
-        textField.autocapitalizationType = .none
-        textField.becomeFirstResponder()
-        return textField
-    }()
-    private lazy var roomsTextLimit : UILabel = {
-        let label = UILabel()
-        label.text = "0/\(maxLength)"
-        label.font = .font(.regular, ofSize: 20)
-        label.textColor = .grey002
-        return label
-    }()
-    private lazy var doneButton: MainButton = {
-        let button = MainButton()
-        button.title = TextLiteral.done
-        button.addTarget(self, action: #selector(didTapDoneButton), for: .touchUpInside)
-        button.isDisabled = true
-        return button
-    }()
     
     // MARK: - init
     
@@ -66,13 +25,15 @@ class CreateNickNameViewController: BaseViewController {
     
     // MARK: - life Cycle
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.setupDelegation()
-        self.setupNotificationCenter()
+    override func loadView() {
+        self.view = self.nicknameView
     }
     
-    // MARK: - API
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - API - 옮길예정
     func requestNickname(nickname: NicknameDTO) {
         Task {
             do {
@@ -88,44 +49,21 @@ class CreateNickNameViewController: BaseViewController {
         }
     }
     
-    override func setupLayout() {
-        view.addSubview(titleLabel)
-        titleLabel.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).inset(66)
-            $0.leading.equalTo(view.safeAreaLayoutGuide).inset(Size.leadingTrailingPadding)
-        }
-        
-        view.addSubview(roomsNameTextField)
-        roomsNameTextField.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(66)
-            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(Size.leadingTrailingPadding)
-            $0.height.equalTo(60)
-        }
-        
-        view.addSubview(roomsTextLimit)
-        roomsTextLimit.snp.makeConstraints {
-            $0.top.equalTo(roomsNameTextField.snp.bottom).offset(10)
-            $0.trailing.equalToSuperview().inset(Size.leadingTrailingPadding)
-        }
-        
-        view.addSubview(doneButton)
-        doneButton.snp.makeConstraints {
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(23)
-            $0.centerX.equalToSuperview()
-        }
+    // MARK: - override
+    
+    override func configureUI() {
+        super.configureUI()
     }
     
-    // MARK: - Seletors
-    
-    @objc private func didTapDoneButton() {
-        if let text = roomsNameTextField.text, !text.isEmpty {
-            nickname = text
-            UserData.setValue(nickname, forKey: .nickname)
-            UserDefaultHandler.setIsSetFcmToken(isSetFcmToken: true)
-            requestNickname(nickname: NicknameDTO(nickname: nickname))
-            presentMainViewController()
-        }
-    }
+//    @objc private func didTapDoneButton() {
+//        if let text = roomsNameTextField.text, !text.isEmpty {
+//            nickname = text
+//            UserData.setValue(nickname, forKey: .nickname)
+//            UserDefaultHandler.setIsSetFcmToken(isSetFcmToken: true)
+//            requestNickname(nickname: NicknameDTO(nickname: nickname))
+//            presentMainViewController()
+//        }
+//    }
     
     private func presentMainViewController() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -135,78 +73,10 @@ class CreateNickNameViewController: BaseViewController {
         present(viewController, animated: true)
     }
     
-    @objc private func keyboardWillShow(notification:NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            UIView.animate(withDuration: 0.2, animations: {
-                self.doneButton.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height + 30)
-            })
-        }
-    }
-    
-    @objc private func keyboardWillHide(notification:NSNotification) {
-        UIView.animate(withDuration: 0.2, animations: {
-            self.doneButton.transform = .identity
-        })
-    }
-    
     override func endEditingView() {
-        if !doneButton.isTouchInside {
-            view.endEditing(true)
-        }
+        self.nicknameView.endEditingView()
     }
     
-    // MARK: - Funtions
+    // MARK: - func
     
-    private func setupDelegation() {
-        roomsNameTextField.delegate = self
-    }
-    
-    private func setupNotificationCenter() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    private func setCounter(count: Int) {
-        if count <= maxLength {
-            roomsTextLimit.text = "\(count)/\(maxLength)"
-        } else {
-            roomsTextLimit.text = "\(maxLength)/\(maxLength)"
-        }
-    }
-    
-    private func checkMaxLength(textField: UITextField, maxLength: Int) {
-        if let text = textField.text {
-            if text.count > maxLength {
-                let endIndex = text.index(text.startIndex, offsetBy: maxLength)
-                let fixedText = text[text.startIndex..<endIndex]
-                textField.text = fixedText + " "
-                
-                DispatchQueue.main.async {
-                    self.roomsNameTextField.text = String(fixedText)
-                }
-            }
-        }
-    }
-    
-    // MARK: - Configure
-    
-    override func configureUI() {
-        super.configureUI()
-    }
-}
-
-// MARK: - Extension
-extension CreateNickNameViewController : UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        roomsNameTextField.resignFirstResponder()
-        return true
-    }
-    
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-        setCounter(count: textField.text?.count ?? 0)
-        checkMaxLength(textField: roomsNameTextField, maxLength: maxLength)
-        
-        let hasText = roomsNameTextField.hasText
-        doneButton.isDisabled = !hasText
-    }
 }
--- Manito/Manito/Screens/CreateNickName/NicknameView.swift
+++ Manito/Manito/Screens/CreateNickName/NicknameView.swift
@@ -13,11 +13,132 @@ final class NicknameView: UIView {
     
     // MARK: - ui components
     
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .font(.regular, ofSize: 34)
+        label.text = self.title
+        return label
+    }()
+    private lazy var nicknameTextField: UITextField = {
+        let textField = UITextField()
+        let attributes = [
+            NSAttributedString.Key.font : UIFont.font(.regular, ofSize: 18)
+        ]
+        textField.backgroundColor = .darkGrey002
+        textField.attributedPlaceholder = NSAttributedString(string: TextLiteral.createNickNameViewControllerAskNickName, attributes:attributes)
+        textField.font = .font(.regular, ofSize: 18)
+        textField.layer.cornerRadius = 10
+        textField.layer.masksToBounds = true
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.textAlignment = .center
+        textField.returnKeyType = .done
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        textField.becomeFirstResponder()
+        textField.delegate = self
+        return textField
+    }()
+    private lazy var textLimitLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0/\(maxLength)"
+        label.font = .font(.regular, ofSize: 20)
+        label.textColor = .grey002
+        return label
+    }()
+    private let doneButton: MainButton = {
+        let button = MainButton()
+        button.title = TextLiteral.done
+        button.isDisabled = true
+        return button
+    }()
+    
     // MARK: - property
+    
+    private let title: String
+    private let maxLength = 5
     
     // MARK: - init
     
+    init(title: String) {
+        self.title = title
+        super.init(frame: .zero)
+        self.setupLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - func
     
+    private func setupLayout() {
+        self.addSubview(self.titleLabel)
+        self.titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).inset(66)
+            $0.leading.equalTo(self.safeAreaLayoutGuide).inset(Size.leadingTrailingPadding)
+        }
+        
+        self.addSubview(self.nicknameTextField)
+        self.nicknameTextField.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(66)
+            $0.leading.trailing.equalToSuperview().inset(Size.leadingTrailingPadding)
+            $0.height.equalTo(60)
+        }
+        
+        self.addSubview(self.textLimitLabel)
+        self.textLimitLabel.snp.makeConstraints {
+            $0.top.equalTo(self.nicknameTextField.snp.bottom).offset(10)
+            $0.trailing.equalToSuperview().inset(Size.leadingTrailingPadding)
+        }
+        
+        self.addSubview(self.doneButton)
+        self.doneButton.snp.makeConstraints {
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).inset(23)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    private func setupNotificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func endEditingView() {
+        if !self.doneButton.isTouchInside {
+            self.endEditing(true)
+        }
+    }
+    
     // MARK: - selector
+    
+    @objc private func keyboardWillShow(notification:NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.doneButton.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height + 30)
+            })
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification:NSNotification) {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.doneButton.transform = .identity
+        })
+    }
+}
+
+extension NicknameView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.nicknameTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        //        setCounter(count: textField.text?.count ?? 0)
+        //        checkMaxLength(textField: roomsNameTextField, maxLength: maxLength)
+        //
+        //        let hasText = roomsNameTextField.hasText
+        //        doneButton.isDisabled = !hasText
+    }
 }
