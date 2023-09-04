eaa25555d9873b2870b1f81849d770e4129beaf9
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-04 22:43:43 +0900

    [CHORE] ViewModel 기본 생성 (#514)

--- Manito/Manito/Screens/ParticipateRoom/ViewModel/ParticipateRoomViewModel.swift
+++ Manito/Manito/Screens/ParticipateRoom/ViewModel/ParticipateRoomViewModel.swift
@@ -5,4 +5,36 @@
 //  Created by 이성호 on 2023/09/04.
 //
 
+import Combine
 import Foundation
+
+final class ParticipateRoomViewModel: ViewModelType {
+    
+    // MARK: - property
+    
+    private let participateRoomService: ParticipateRoomService
+    private var cancellable = Set<AnyCancellable>()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(from input: Input) -> Output {
+        return Output()
+    }
+    
+    // MARK: - init
+    
+    init(participateRoomService: ParticipateRoomService) {
+        self.participateRoomService = participateRoomService
+    }
+    
+    // MARK: - func
+    
+    // MARK: - network
+    
+}
