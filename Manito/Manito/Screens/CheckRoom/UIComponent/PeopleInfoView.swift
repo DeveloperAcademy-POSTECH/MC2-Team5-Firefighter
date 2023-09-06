ca4c59f2466a3f956c973fc1b687dad59d8a92ed
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-06 15:59:19 +0900

    [CHORE] 컨벤션 통일 (#514)

--- Manito/Manito/Screens/CheckRoom/UIComponent/PeopleInfoView.swift
+++ Manito/Manito/Screens/CheckRoom/UIComponent/PeopleInfoView.swift
@@ -27,6 +27,7 @@ final class PeopleInfoView: UIView {
         self.setupLayout()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- Manito/Manito/Screens/CheckRoom/UIComponent/RoomInfoView.swift
+++ Manito/Manito/Screens/CheckRoom/UIComponent/RoomInfoView.swift
@@ -32,6 +32,7 @@ final class RoomInfoView: UIView {
         self.setupLayout()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
