b0099e6185553067f7d7154866298ec6f1d91be8
chemi <wbsldj7645@naver.com>
2023-09-11 18:00:16 +0900

    [ADD] Navigationable 프로토콜을 따르는 vc에 버튼 추가 코드 작성 (#531)

--- Manito/Manito/Presentation/Common/Base/Navigationable.swift
+++ Manito/Manito/Presentation/Common/Base/Navigationable.swift
@@ -5,4 +5,53 @@
 //  Created by Mingwan Choi on 2023/09/11.
 //
 
-import Foundation
+import UIKit
+
+protocol Navigationable {
+    func setupNavigation()
+}
+
+extension Navigationable where Self: UIViewController {
+    func setupNavigation() {
+        self.setupBackButton()
+        self.setupNavigationBar()
+    }
+    
+    private func backButtonItem() -> UIBarButtonItem {
+        let button = BackButton()
+        let buttonAction = UIAction { [weak self] _ in
+            self?.back()
+        }
+        button.addAction(buttonAction, for: .touchUpInside)
+        let leftOffsetBackButton = self.removeBarButtonItemOffset(with: button, offsetX: 10)
+        let backButton = self.makeBarButtonItem(with: leftOffsetBackButton)
+        return backButton
+    }
+    
+    private func setupBackButton() {
+        let backButton = self.backButtonItem()
+        self.navigationItem.leftBarButtonItem = backButton
+    }
+    
+    private func back() {
+        if let navigation = self.navigationController {
+            navigation.popViewController(animated: true)
+        }
+    }
+    
+    private func setupNavigationBar() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        let appearance = UINavigationBarAppearance()
+        let font = UIFont.font(.regular, ofSize: 14)
+        let largeFont = UIFont.font(.regular, ofSize: 34)
+        
+        appearance.titleTextAttributes = [.font: font]
+        appearance.largeTitleTextAttributes = [.font: largeFont]
+        appearance.shadowColor = .clear
+        appearance.backgroundColor = .backgroundGrey
+        
+        navigationBar.standardAppearance = appearance
+        navigationBar.compactAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
+    }
+}
