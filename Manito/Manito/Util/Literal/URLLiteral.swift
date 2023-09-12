fe4bdcae9b4cc24c322854742a98b831d61c9155
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-09-12 18:33:16 +0900

    [ADD] Localizable Script 추가(#530)

--- Manito/Manito/Util/Literal/URLLiteral.swift
+++ Manito/Manito/Util/Literal/URLLiteral.swift
@@ -13,4 +13,9 @@ enum URLLiteral {
     
     static let personalInfomationUrl: String = "https://torpid-spy-8e4.notion.site/767e80eea1734539aead3b814016b361"
     static let termsOfServiceUrl: String = "https://torpid-spy-8e4.notion.site/445bd6a8c8dc459d915158935dcc3298"
+
+    // MARK: - google sheet
+
+    static let localizableURL: String = "https://docs.google.com/spreadsheets/d/1eccIkg8KM8tj-HR1FW_6b-F50asLr-DNj9hP_hvAYc8/export?format=csv"
+    static let filePath: String = "/Manito/Manito/Util/Literal/en.lproj/Localizable.strings"
 }
