3a2c1fdd745112ada48955421575ffa840326605
chemi <wbsldj7645@naver.com>
2023-09-19 23:05:45 +0900

    [ADD] isToday에 대한 테스트 코드 추가 (#543)

--- Manito/ManitoTests/Util/Extension/Type/Date+ExtensionTest.swift
+++ Manito/ManitoTests/Util/Extension/Type/Date+ExtensionTest.swift
@@ -6,30 +6,52 @@
 //
 
 import XCTest
+@testable import Manito
 
 final class Date_ExtensionTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_isToday_변수가_존재하는가() {
+        let sut = Date()
+        
+        let _ = sut.isToday
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_isToday_오늘날짜에_올바르게_반환하는가() {
+        // given
+        let date = Date().toFullString
+        let today = date.toFullDate!
+        
+        // when
+        let isToday = today.isToday
+        
+        // then
+        XCTAssertTrue(isToday)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_isToday_과거날짜에_올바르게_반환하는가() {
+        // given
+        let oneTimeInterval: TimeInterval = 86400
+        let date = Date().toFullString
+        let today = date.toFullDate!
+        let sut = today - oneTimeInterval
+        
+        // when
+        let isToday = sut.isToday
+        
+        // then
+        XCTAssertFalse(isToday)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_isToday_미래날짜에_올바르게_반환하는가() {
+        // given
+        let oneTimeInterval: TimeInterval = 86400
+        let date = Date().toFullString
+        let today = date.toFullDate!
+        let sut = today + oneTimeInterval
+        
+        // when
+        let isToday = sut.isToday
+        
+        // then
+        XCTAssertFalse(isToday)
     }
-
 }
