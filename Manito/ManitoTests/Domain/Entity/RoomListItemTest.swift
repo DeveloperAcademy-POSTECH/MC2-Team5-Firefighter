d8fbae8e8c956d15cfc4dbd347f7e8aade164fbb
chemi <wbsldj7645@naver.com>
2023-09-19 22:45:15 +0900

    [ADD] isStartDatePast에 대한 테스트 코드 추가 (#543)

--- Manito/ManitoTests/Domain/Entity/RoomListItemTest.swift
+++ Manito/ManitoTests/Domain/Entity/RoomListItemTest.swift
@@ -5,6 +5,7 @@
 //  Created by Mingwan Choi on 2023/09/18.
 //
 
+import Foundation
 import XCTest
 @testable import Manito
 
@@ -35,5 +36,49 @@ final class RoomListItemTest: XCTestCase {
         
         // then
         XCTAssertNotEqual(dateRangeText, "2023.01.01  ~  2023.01.05")
+    }
+    
+    func test_isStartDatePast_변수가_있는가() {
+        let sut = RoomListItem.testRoomListItem
+        
+        let _ = sut.isStartDatePast
+    }
+    
+    func test_isStartDatePast_과거날짜에대해_올바르게_반환하는가() {
+        // given
+        let sut = RoomListItem.testRoomListItem
+        
+        // when
+        let isPast = sut.isStartDatePast
+        
+        // then
+        XCTAssertEqual(isPast, true)
+    }
+    
+    func test_isStartDatePast_미래날짜에대해_올바르게_반환하는가() {
+        // given
+        let sut = RoomListItem(id: 0, title: "", state: "", capacity: 0, startDate: "2030.01.01", endDate: "2030.01.05")
+        
+        // when
+        let isPast = sut.isStartDatePast
+        
+        // then
+        XCTAssertEqual(isPast, false)
+    }
+    
+    func test_isStartDatePast_오늘날짜에대해_올바르게_반환하는가() {
+        // given
+        let today = Date()
+        let ondDay: TimeInterval = 86400
+        let todayAfterFiveDays = Date() + ondDay
+        let todayString = today.toFullString
+        let endDateString = todayAfterFiveDays.toFullString
+        let sut = RoomListItem(id: 0, title: "", state: "", capacity: 0, startDate: todayString, endDate: endDateString)
+        
+        // when
+        let isPast = sut.isStartDatePast
+        
+        // then
+        XCTAssertEqual(isPast, false)
     }
 }
