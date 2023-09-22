2ba81e155204fc26306a231b0cb8f13e22fd4cc2
chemi <wbsldj7645@naver.com>
2023-09-22 13:31:17 +0900

    [CHORE] (옵션) 제거 (#541)

--- Manito/Manito/Util/Extension/Type/String+DateFormatter.swift
+++ Manito/Manito/Util/Extension/Type/String+DateFormatter.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension String {
-    /// String 값을 yy.MM.dd 형식의 Date 값으로 변환 (옵션)
+    /// String 값을 yy.MM.dd 형식의 Date 값으로 변환
     var toDefaultDate: Date? {
         let formatter = DateFormatter()
         formatter.dateFormat = "yy.MM.dd"
@@ -16,7 +16,7 @@ extension String {
         return formatter.date(from: self)
     }
     
-    /// String 값을 yyyy.MM.dd 형식의 Date 값으로 변환 (옵션)
+    /// String 값을 yyyy.MM.dd 형식의 Date 값으로 변환
     var toFullDate: Date? {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy.MM.dd"
