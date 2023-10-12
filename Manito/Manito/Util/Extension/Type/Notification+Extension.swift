6528cce6e35bffe669f5314a5d18f1cf86e604a2
chemi <wbsldj7645@naver.com>
2023-10-12 21:13:37 +0900

    [CHORE] notification 삭제 (#550)

--- Manito/Manito/Util/Extension/Type/Notification+Extension.swift
+++ Manito/Manito/Util/Extension/Type/Notification+Extension.swift
@@ -5,8 +5,8 @@
 //  Created by 이성호 on 2022/06/18.
 //
 
-import Foundation
-
-extension Notification.Name {
-    static let createRoomInvitedCode = Notification.Name("CreateRoomInvitedCode")
-}
+//import Foundation
+//
+//extension Notification.Name {
+//    static let createRoomInvitedCode = Notification.Name("CreateRoomInvitedCode")
+//}
