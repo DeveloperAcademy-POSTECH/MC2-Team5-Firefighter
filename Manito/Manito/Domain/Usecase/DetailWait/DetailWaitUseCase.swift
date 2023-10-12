6d84f62e257b69048066bfcc149b14d458cfcd76
chemi <wbsldj7645@naver.com>
2023-10-12 17:29:58 +0900

    [CHORE] roomIndex -> roomId 수정 (#550)

--- Manito/Manito/Domain/Usecase/DetailWait/DetailWaitUseCase.swift
+++ Manito/Manito/Domain/Usecase/DetailWait/DetailWaitUseCase.swift
@@ -9,61 +9,58 @@ import Combine
 import Foundation
 
 protocol DetailWaitUseCase {
-    var roomIndex: Int { get set }
     var roomInformation: RoomInfo { get set }
     
-    func fetchRoomInformaion(roomIndex: String) async throws -> RoomInfoDTO
-    func patchStartManitto(roomIndex: String) async throws -> UserInfoDTO
-    func deleteRoom(roomIndex: String) async throws -> Int
-    func deleteLeaveRoom(roomIndex: String) async throws -> Int
+    func fetchRoomInformaion(roomId: String) async throws -> RoomInfoDTO
+    func patchStartManitto(roomId: String) async throws -> UserInfoDTO
+    func deleteRoom(roomId: String) async throws -> Int
+    func deleteLeaveRoom(roomId: String) async throws -> Int
 }
 
 final class DetailWaitUseCaseImpl: DetailWaitUseCase {
     
     // MARK: - property
     
-    var roomIndex: Int
     @Published var roomInformation: RoomInfo = RoomInfo.emptyRoom
     
     private let repository: DetailRoomRepository
     
-    init(roomIndex: Int, repository: DetailRoomRepository) {
-        self.roomIndex = roomIndex
+    init(repository: DetailRoomRepository) {
         self.repository = repository
     }
     
     // MARK: - func
     
-    func fetchRoomInformaion(roomIndex: String) async throws -> RoomInfoDTO {
+    func fetchRoomInformaion(roomId: String) async throws -> RoomInfoDTO {
         do {
-            let roomInformation = try await self.repository.fetchRoomInfo(roomId: roomIndex)
+            let roomInformation = try await self.repository.fetchRoomInfo(roomId: roomId)
             return roomInformation
         } catch {
             throw NetworkError.unknownError
         }
     }
     
-    func patchStartManitto(roomIndex: String) async throws -> UserInfoDTO {
+    func patchStartManitto(roomId: String) async throws -> UserInfoDTO {
         do {
-            let userInformation = try await self.repository.patchStartManitto(roomId: roomIndex)
+            let userInformation = try await self.repository.patchStartManitto(roomId: roomId)
             return userInformation
         } catch {
             throw NetworkError.unknownError
         }
     }
     
-    func deleteRoom(roomIndex: String) async throws -> Int {
+    func deleteRoom(roomId: String) async throws -> Int {
         do {
-            let statusCode = try await self.repository.deleteRoom(roomId: roomIndex)
+            let statusCode = try await self.repository.deleteRoom(roomId: roomId)
             return statusCode
         } catch {
             throw NetworkError.unknownError
         }
     }
     
-    func deleteLeaveRoom(roomIndex: String) async throws -> Int {
+    func deleteLeaveRoom(roomId: String) async throws -> Int {
         do {
-            let statusCode = try await self.repository.deleteLeaveRoom(roomId: roomIndex)
+            let statusCode = try await self.repository.deleteLeaveRoom(roomId: roomId)
             return statusCode
         } catch {
             throw NetworkError.unknownError
