06d265c8157daa23ef11cc24fc4ddc125e0d0deb
chemi <wbsldj7645@naver.com>
2023-10-12 23:40:24 +0900

    [ADD] MockUsecase 추가 (#550)

---
+++ Manito/ManitoTests/Domain/Usecase/DetailWaitUsecaseTest.swift
@@ -0,0 +1,45 @@
+//
+//  DetailWaitUsecaseTest.swift
+//  ManitoTests
+//
+//  Created by Mingwan Choi on 10/12/23.
+//
+
+import XCTest
+@testable import Manito
+
+final class DetailWaitUsecaseTest: XCTestCase {
+    private var mockUsecase: DetailWaitUseCase!
+    
+    override func setUp() {
+        self.mockUsecase = MockDetailWaitUsecase()
+    }
+    
+    override func tearDown() {
+        self.mockUsecase = nil
+    }
+}
+
+final class MockDetailWaitUsecase: DetailWaitUseCase {
+    var roomInformation: Manito.RoomInfo = .testRoom
+    
+    func fetchRoomInformaion(roomId: String) async throws -> RoomInfoDTO {
+        return .testRoomDTO
+    }
+    
+    func patchStartManitto(roomId: String) async throws -> UserInfoDTO {
+        return .testUserManittee
+    }
+    
+    func deleteRoom(roomId: String) async throws -> Int {
+        try await Task.sleep(nanoseconds: 3_000)
+        
+        return 200
+    }
+    
+    func deleteLeaveRoom(roomId: String) async throws -> Int {
+        try await Task.sleep(nanoseconds: 3_000)
+        
+        return 200
+    }
+}
