a133159eb808d68ee6571e294a805f00d205ca3d
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-10-13 22:31:02 +0900

    [MOVE] SplashVC 이동 및 SplashView 생성(#561)

---
+++ Manito/Manito/Presentation/Scene/SplashScene/View/SplashView.swift
@@ -0,0 +1,35 @@
+//
+//  SplashView.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 10/13/23.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SplashView: UIView, BaseViewType {
+
+    // MARK: - init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.baseInit()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - func
+    
+    func setupLayout() {
+        <#code#>
+    }
+    
+    func configureUI() {
+        <#code#>
+    }
+}
