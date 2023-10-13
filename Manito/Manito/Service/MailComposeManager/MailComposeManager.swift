973038cc381ee6b2bfea7c9ef6f8b32f5e96ffb8
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-10-13 16:27:27 +0900

    [ADD] MailComposeManager 생성(#556)

---
+++ Manito/Manito/Service/MailComposeManager/MailComposeManager.swift
@@ -0,0 +1,57 @@
+//
+//  MailComposeManager.swift
+//  Manito
+//
+//  Created by SHIN YOON AH on 10/13/23.
+//
+
+import MessageUI
+
+final class MailComposeManager: NSObject {
+    
+    // MARK: - property
+    
+    private var defaultMailController: MFMailComposeViewController {
+        let controller = MFMailComposeViewController()
+        let aenittoEmail = TextLiteral.Mail.aenittoEmail.localized()
+        controller.mailComposeDelegate = self
+        controller.setToRecipients([aenittoEmail])
+        return controller
+    }
+    
+    weak var viewController: UIViewController?
+    
+    // MARK: - func
+    
+    func sendMail(title: String, content: String) {
+        if MFMailComposeViewController.canSendMail() {
+            self.showMail(title: title, content: content)
+        } else {
+            self.showErrorAlert()
+        }
+    }
+    
+    // MARK: - Private - func
+    
+    private func showMail(title: String, content: String) {
+        self.defaultMailController.setSubject(title)
+        self.defaultMailController.setMessageBody(content, isHTML: false)
+        self.viewController?.present(self.defaultMailController, animated: true)
+    }
+    
+    private func showErrorAlert() {
+        let alertController = UIAlertController(title: TextLiteral.Mail.Error.title.localized(),
+                                                message: TextLiteral.Mail.Error.message.localized(),
+                                                preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: TextLiteral.Common.confirm.localized(), style: .default)
+        alertController.addAction(confirmAction)
+        self.viewController?.present(alertController, animated: true)
+    }
+}
+
+// MARK: - MFMailComposeViewControllerDelegate
+extension MailComposeManager: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+}
