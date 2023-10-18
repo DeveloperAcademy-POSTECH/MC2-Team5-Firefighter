abdd6fe9ee3ade199fa0c6457c35a38026801fc6
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-10-18 13:49:04 +0900

    [FEAT] Open Manitto Usecase 구성(#565)

--- Manito/Manito/Domain/Error/DetailScene/DetailUsecaseError.swift
+++ Manito/Manito/Domain/Error/DetailScene/DetailUsecaseError.swift
@@ -9,12 +9,11 @@ import Foundation
 
 enum DetailUsecaseError: LocalizedError {
     case failedToFetchMemory
+    case failedToFetchFriend
 }
 
 extension DetailUsecaseError {
     var errorDescription: String? {
-        switch self {
-        case .failedToFetchMemory: return TextLiteral.Common.Error.networkServer.localized()
-        }
+        return TextLiteral.Common.Error.networkServer.localized()
     }
 }
