6502e05419c8c4501e671c822626a379ded97cb0
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-10-19 16:28:34 +0900

    [CHORE] Cell 코드 수정(#571)

--- Manito/Manito/Presentation/Scene/DetailScene/Detail/View/Cell/FriendCollectionViewCell.swift
+++ Manito/Manito/Presentation/Scene/DetailScene/Detail/View/Cell/FriendCollectionViewCell.swift
@@ -7,27 +7,75 @@
 
 import UIKit
 
-class FriendCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var friendListBackView: UIView!
-    @IBOutlet weak var friendListImageView: UIImageView!
-    @IBOutlet weak var friendListNameLabel: UILabel!
+import SnapKit
+
+final class FriendCollectionViewCell: UICollectionViewCell, BaseViewType {
+
+    // MARK: - ui component
     
-    func setupFont() {
-        friendListNameLabel.font = .font(.regular, ofSize: 15)
+    private let friendBackView: UIView = {
+        let view = UIView()
+        view.makeBorderLayer(color: .white)
+        view.layer.cornerRadius = 49
+        return view
+    }()
+    private let friendImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 45
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    private let friendNicknameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .font(.regular, ofSize: 15)
+        return label
+    }()
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.baseInit()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    func setupViewLayer() {
-        friendListBackView.makeBorderLayer(color: .white)
-        friendListImageView.layer.cornerRadius = 45
-        friendListBackView.layer.cornerRadius = 49
+    // MARK: - base view
+    
+    func setupLayout() {
+        self.addSubview(self.friendBackView)
+        self.friendBackView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(16)
+            $0.centerX.equalToSuperview()
+        }
+        
+        self.friendBackView.addSubview(self.friendImageView)
+        self.friendImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(4)
+            $0.width.height.equalTo(90)
+        }
+        
+        self.addSubview(self.friendNicknameLabel)
+        self.friendNicknameLabel.snp.makeConstraints {
+            $0.top.equalTo(self.friendBackView.snp.bottom).offset(14)
+            $0.leading.trailing.equalToSuperview().inset(10)
+            $0.height.equalTo(16)
+        }
     }
     
-    func setFriendName(name: String) {
-        friendListNameLabel.text = name
+    func configureUI() {
+        self.backgroundColor = .white.withAlphaComponent(0.3)
+        self.makeBorderLayer(color: .white)
     }
     
-    func setFriendImage(index: Int) {
-        friendListBackView.backgroundColor = DefaultCharacterType.allCases[index].backgroundColor
-        friendListImageView.image = DefaultCharacterType.allCases[index].image
+    // MARK: - func
+    
+    func configureCell(name: String, colorIndex: Int) {
+        self.friendNicknameLabel.text = name
+        self.friendBackView.backgroundColor = DefaultCharacterType.allCases[colorIndex].backgroundColor
+        self.friendImageView.image = DefaultCharacterType.allCases[colorIndex].image
     }
 }
