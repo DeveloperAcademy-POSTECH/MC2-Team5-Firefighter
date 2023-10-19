80588304a84d911c77062178c1b27db1a14b651f
YoonAh-dev <tlsdbsdk05250@gmail.com>
2023-10-19 13:48:12 +0900

    [FEAT] List CollectionView Layout 잡기(#571)

--- Manito/Manito/Presentation/Scene/DetailScene/Detail/View/FriendListView.swift
+++ Manito/Manito/Presentation/Scene/DetailScene/Detail/View/FriendListView.swift
@@ -11,11 +11,22 @@ import SnapKit
 
 final class FriendListView: UIView, BaseViewType {
     
+    private enum ConstantSize {
+        static let groupInterItemSpacing: CGFloat = 14
+        static let sectionContentInset: NSDirectionalEdgeInsets = NSDirectionalEdgeInsets(
+            top: 18.0,
+            leading: SizeLiteral.leadingTrailingPadding,
+            bottom: 18.0,
+            trailing: SizeLiteral.leadingTrailingPadding
+        )
+    }
+    
     // MARK: - ui component
     
-    private let friendListCollectionView: UICollectionView = {
-        let collectionView = UICollectionView()
+    private lazy var friendListCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout())
         collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
         collectionView.register(cell: FriendCollectionViewCell.self,
                                 forCellWithReuseIdentifier: FriendCollectionViewCell.className)
         return collectionView
@@ -36,7 +47,11 @@ final class FriendListView: UIView, BaseViewType {
     // MARK: - base func
     
     func setupLayout() {
-        
+        self.addSubview(self.friendListCollectionView)
+        self.friendListCollectionView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
     
     func configureUI() {
@@ -47,6 +62,26 @@ final class FriendListView: UIView, BaseViewType {
 // MARK: - UICollectionViewLayout
 extension FriendListView {
     private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { index, environment -> NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.5),
+                heightDimension: .fractionalWidth(0.5)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(0.5)
+            )
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = ConstantSize.sectionContentInset
+            section.interGroupSpacing = ConstantSize.groupInterItemSpacing
+            
+            return section
+        }
         
+        return layout
     }
 }
