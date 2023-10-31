43e8207bb9b7ae7c263c1771fc42c1c6500fb3e1
chemi <wbsldj7645@naver.com>
2023-10-31 23:22:04 +0900

    [ADD] Usecase 추가 (#586)

--- Manito/Manito/Domain/Usecase/DetailWait/DetailEditUsecase.swift
+++ Manito/Manito/Domain/Usecase/DetailWait/DetailEditUsecase.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+protocol DetailEditUsecase {
+    var roomInformation: RoomInfo { get set }
+    
+    func changeRoomInformation(roomDto: CreatedRoomInfoRequestDTO) async throws -> Int
+}
+
+final class DetailEditUsecaseImpl: DetailEditUsecase {
+    
+    @Published var roomInformation: RoomInfo
+    
+    let repository: DetailRoomRepository
+    
+    init(roomInformation: RoomInfo, repository: DetailRoomRepository) {
+        self.roomInformation = roomInformation
+        self.repository = repository
+    }
+    
+    func changeRoomInformation(roomDto: CreatedRoomInfoRequestDTO) async throws -> Int {
+        let statusCode = try await self.repository.putRoomInfo(roomId: self.roomInformation.roomInformation.id.description,
+                                                               roomInfo: roomDto)
+        return statusCode
+    }
+}
