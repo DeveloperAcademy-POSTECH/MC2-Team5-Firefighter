a6b759785fda8a017ffbb511e90e1b6674971b6b
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-11-01 17:00:46 +0900

    [CHORE] CreateRoomInfo 엔티티 생성 (#585)

---
+++ Manito/Manito/Domain/Entity/CreateRoomInfo.swift
@@ -0,0 +1,15 @@
+//
+//  CreateRoom.swift
+//  Manito
+//
+//  Created by 이성호 on 11/1/23.
+//
+
+import Foundation
+
+struct CreateRoomInfo {
+    let title: String
+    let capacity: Int
+    let startDate: String
+    let endDate: String
+}
