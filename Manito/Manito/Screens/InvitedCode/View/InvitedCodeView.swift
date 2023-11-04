e8b4768b422fc6947c120cf3b1ea513e78643e17
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-11-04 11:04:56 +0900

    [ADD] View 생성 (594)

---
+++ Manito/Manito/Screens/InvitedCode/View/InvitedCodeView.swift
@@ -0,0 +1,40 @@
+//
+//  InvitedCodeView.swift
+//  Manito
+//
+//  Created by 이성호 on 11/4/23.
+//
+
+import Combine
+import UIKit
+
+import SnapKit
+
+final class InvitedCodeView: UIView, BaseViewType {
+    
+    // MARK: - ui components
+    
+    // MARK: - property
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - func
+    
+    func setupLayout() {
+        
+    }
+    
+    func configureUI() {
+        
+    }
+    
+    
+}
