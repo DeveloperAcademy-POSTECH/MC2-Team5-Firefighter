64ad8ce6879def8229649defc91b56b7d1061fba
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-11-04 11:06:30 +0900

    [ADD] ViewModel 생성 (#594)

---
+++ Manito/Manito/Screens/InvitedCode/ViewModel/InvitedCodeViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  InvitedCodeViewModel.swift
+//  Manito
+//
+//  Created by 이성호 on 11/4/23.
+//
+
+import Foundation
+
+import Combine
+
+final class InvitedCodeViewModel: BaseViewModelType {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    // MARK: - property
+    
+    // MARK: - init
+    
+    // MARK: - func
+    
+    func transform(from input: Input) -> Output {
+        return Output()
+    }
+}
